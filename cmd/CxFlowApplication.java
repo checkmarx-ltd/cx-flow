fb3fde8f937ee1bea75aa4c73ea90dc7e6a1e24f
Ken McDonald <ken@custodela.com>
2019-04-30 21:21:10 -0400

    Missing import statement for SpringBoot

--- cmd/CxFlowApplication.java
+++ cmd/CxFlowApplication.java
@@ -14,6 +14,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import java.beans.ConstructorProperties;
 import java.io.File;
