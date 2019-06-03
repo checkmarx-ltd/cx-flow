53850a213ab592fae16f74d2c0b82edd1833ccd8
Ken McDonald <ken.mcdonald@checkmarx.com>
2019-06-03 09:22:25 -0400

    More changes associated with 1 application entry point for both cmd and web versions

--- src/main/java/com/checkmarx/flow/CxFlowApplication.java
+++ src/main/java/com/checkmarx/flow/CxFlowApplication.java
@@ -1,14 +1,24 @@
 package com.checkmarx.flow;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableAsync
 public class CxFlowApplication {
 	public static void main(String[] args) {
-		if(args != null) {
+		boolean web = Arrays.asList(args).contains("--web");
+
+		if(args.length > 0 && !web){
+			SpringApplication app = new SpringApplication(CxFlowApplication.class);
+			app.setWebApplicationType(WebApplicationType.NONE);
+			app.run(args);
+		}
+		else{
 			SpringApplication.run(CxFlowApplication.class, args);
 		}
 	}
