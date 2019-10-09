d67debc44ae0980c91608f74d24aeb5a13fe7a6d
Ken McDonald <ken.mcdonald@checkmarx.com>
2019-10-09 11:18:02 -0400

    Update Abstract class to include ScanRequest object in the signature of functions

--- src/main/java/com/checkmarx/flow/service/RepoService.java
+++ src/main/java/com/checkmarx/flow/service/RepoService.java
@@ -1,18 +1,20 @@
 package com.checkmarx.flow.service;
 
+import com.checkmarx.flow.dto.ScanRequest;
 import com.checkmarx.flow.dto.Sources;
 import com.checkmarx.sdk.dto.CxConfig;
+import com.checkmarx.sdk.exception.CheckmarxException;
 import com.checkmarx.sdk.utils.ScanUtils;
 
 public abstract class RepoService {
 
     public abstract Sources getRepoContent();
 
-    public CxConfig getCxConfigOverride(){
+    public CxConfig getCxConfigOverride(ScanRequest request) throws CheckmarxException {
         return null;
     }
 
-    public String profileSource(Sources sources){
+    public String profileSource(Sources sources) throws CheckmarxException {
         //TODO iterate through fingerprint/profiles (TBD) and find the first match, and return appropriate "profile" aka Scanning preset
         return null;
     }
