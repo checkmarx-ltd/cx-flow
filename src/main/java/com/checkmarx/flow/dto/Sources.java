e2ca5a0ec501834f1a7bf1ed42351fc82ee60024
Ken McDonald <ken.mcdonald@checkmarx.com>
2019-11-07 09:59:13 -0500

    #37 adding classes to support GitHub Automated profiling / crawling to support automated preset selection

--- src/main/java/com/checkmarx/flow/dto/Sources.java
+++ src/main/java/com/checkmarx/flow/dto/Sources.java
@@ -1,9 +1,13 @@
 package com.checkmarx.flow.dto;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Sources {
     List<Source> sources;
+    Map<String, Integer> languageStats;
 
     public List<Source> getSources() {
         return sources;
@@ -11,6 +15,24 @@ public class Sources {
 
     public void setSources(List<Source> sources) {
         this.sources = sources;
+    }
+
+    public void addSource(String path, String file){
+        if(sources == null){
+            sources = new ArrayList<>();
+        }
+        Source s = new Source();
+        s.setFile(file);
+        s.setPath(path);
+        sources.add(s);
+    }
+
+    public Map<String, Integer> getLanguageStats() {
+        return languageStats;
+    }
+
+    public void setLanguageStats(Map<String, Integer> languageStats) {
+        this.languageStats = languageStats;
     }
 
     public static class Source{
