b49a2e49cfd655f57ebf47c9973a51f96c0ebf7f
Ken McDonald <ken@custodela.com>
2019-11-19 10:57:41 -0500

    Adding GitLab Spring boot Test Context

--- src/test/java/com/checkmarx/flow/service/GitLabServiceTest.java
+++ src/test/java/com/checkmarx/flow/service/GitLabServiceTest.java
@@ -1,7 +1,15 @@
 package com.checkmarx.flow.service;
 
+import com.checkmarx.flow.config.GitLabProperties;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@Import(GitLabProperties.class)
+@SpringBootTest
 public class GitLabServiceTest {
 
     @Test
