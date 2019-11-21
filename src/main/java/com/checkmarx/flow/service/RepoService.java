fd36d8630b7943f18979ee4ba1e82bfdecf67717
Ken McDonald <ken.mcdonald@checkmarx.com>
2019-11-20 19:25:03 -0500

    Cleanup of unnccessary or unused logic

--- src/main/java/com/checkmarx/flow/service/RepoService.java
+++ src/main/java/com/checkmarx/flow/service/RepoService.java
@@ -10,12 +10,8 @@ public abstract class RepoService {
 
     public abstract Sources getRepoContent(ScanRequest request);
 
-    public CxConfig getCxConfigOverride(ScanRequest request) throws CheckmarxException {
+    public CxConfig getCxConfigOverride(ScanRequest request) {
         return null;
     }
-
-    public String profileSource(Sources sources) throws CheckmarxException {
-        //TODO iterate through fingerprint/profiles (TBD) and find the first match, and return appropriate "profile" aka Scanning preset
-        return null;
-    }
+    
 }
