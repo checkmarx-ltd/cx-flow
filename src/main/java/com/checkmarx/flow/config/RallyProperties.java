d7822f0b3fae1cc2355c70bc4e9bbe0370d0d85d
Jeff Armstrong <jeff.armstrong@checkmarx.com>
2020-02-09 22:32:57 -0800

    - Added values to RallyProperties.java for to apply labels to Rally issues - Removed Cx before Rally issue Titles - Rally bug tracker now uses labels to locate CxFlow issues instead of issue titles.

--- src/main/java/com/checkmarx/flow/config/RallyProperties.java
+++ src/main/java/com/checkmarx/flow/config/RallyProperties.java
@@ -10,20 +10,54 @@ import org.springframework.validation.annotation.Validated;
 public class RallyProperties extends RepoProperties {
     private String rallyWorkspaceId;
     private String rallyProjectId;
+    private String appLabelPrefix = "app";
+    private String ownerLabelPrefix = "owner";
+    private String repoLabelPrefix = "repo";
+    private String branchLabelPrefix = "branch";
 
     public String getRallyWorkspaceId() {
         return rallyWorkspaceId;
+    }
+
+    public String getRallyProjectId() { return rallyProjectId; }
+
+    public String getOwnerLabelPrefix() {
+        return this.ownerLabelPrefix;
+    }
+
+    public String getRepoLabelPrefix() {
+        return this.repoLabelPrefix;
+    }
+
+    public String getBranchLabelPrefix() {
+        return this.branchLabelPrefix;
+    }
+
+    public String getAppLabelPrefix() {
+        return this.appLabelPrefix;
     }
 
     public void setRallyWorkspaceId(String rallyWorkspaceId) {
         this.rallyWorkspaceId = rallyWorkspaceId;
     }
 
-    public String getRallyProjectId() {
-        return rallyProjectId;
-    }
-
     public void setRallyProjectId(String rallyProjectId) {
         this.rallyProjectId = rallyProjectId;
+    }
+
+    public void setOwnerLabelPrefix(String ownerLabelPrefix) {
+        this.ownerLabelPrefix = ownerLabelPrefix;
+    }
+
+    public void setRepoLabelPrefix(String repoLabelPrefix) {
+        this.repoLabelPrefix = repoLabelPrefix;
+    }
+
+    public void setBranchLabelPrefix(String branchLabelPrefix) {
+        this.branchLabelPrefix = branchLabelPrefix;
+    }
+
+    public void setAppLabelPrefix(String appLabelPrefix) {
+        this.appLabelPrefix = appLabelPrefix;
     }
 }
