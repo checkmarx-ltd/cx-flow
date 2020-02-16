b19f16219fa5ea7de8f4b38e5991919dc6a0430b
AlexeyK <alexey.kononov@checkmarx.com>
2020-02-16 15:12:04 +0200

    Added a high load scenario for the WebHooks feature.

---
+++ src/test/java/com/checkmarx/flow/cucumber/component/webhook/WebhookTestRunner.java
@@ -0,0 +1,12 @@
+package com.checkmarx.flow.cucumber.component.webhook;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/cucumber/features/componentTests/webhook.feature",
+        tags = "@WebHookFeature and not @Skip")
+public class WebhookTestRunner {
+}
