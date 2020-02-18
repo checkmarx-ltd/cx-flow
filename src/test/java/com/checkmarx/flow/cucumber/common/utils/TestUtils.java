604af89004bd3945deba89a08557d2bce7a25f38
AlexeyK <alexey.kononov@checkmarx.com>
2020-02-18 14:53:33 +0200

    Implemented 'High load' WebHook scenario.

--- src/test/java/com/checkmarx/flow/cucumber/common/utils/TestUtils.java
+++ src/test/java/com/checkmarx/flow/cucumber/common/utils/TestUtils.java
@@ -1,12 +1,22 @@
 package com.checkmarx.flow.cucumber.common.utils;
 
 import com.checkmarx.flow.CxFlowRunner;
+import com.checkmarx.flow.cucumber.component.parse.TestContext;
 import org.springframework.boot.DefaultApplicationArguments;
 
+import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Paths;
 import java.util.Stack;
 
 public class TestUtils {
+    public static InputStream getResourceAsStream(String relativePath) {
+        String srcResourcePath = Paths.get(TestContext.CUCUMBER_DATA_DIR, relativePath)
+                .toString();
+
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        return classLoader.getResourceAsStream(srcResourcePath);
+    }
 
     private interface Revertible {
         void revert();
--- src/test/resources/cucumber/features/componentTests/webhook.feature
+++ src/test/resources/cucumber/features/componentTests/webhook.feature
@@ -1,9 +1,7 @@
-@WebHookFeature @ComponentTest
-Feature: Check Component tests Webhook functionality
+@Skip @WebHookFeature @ComponentTest
+Feature: Processing WebHook requests from version control providers
 
   Scenario: High load on CxFlow due to frequent WebHook requests from GitHub
-    Given version control provider is GitHub
     Given CxFlow is running as a service
-    And webhook is configured for push event
     When GitHub sends WebHook requests to CxFlow 2 times per second
     Then each of the requests is answered in at most 500 ms
