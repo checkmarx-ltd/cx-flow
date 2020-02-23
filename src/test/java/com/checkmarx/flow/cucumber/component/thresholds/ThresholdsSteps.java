f4bab6a0c769ab645d6df3fb21aa4a4f68c6dd8f
AlexeyK <alexey.kononov@checkmarx.com>
2020-02-23 10:57:45 +0200

    Threshold scenario and test setup.

---
+++ src/test/java/com/checkmarx/flow/cucumber/component/thresholds/ThresholdsSteps.java
@@ -0,0 +1,9 @@
+package com.checkmarx.flow.cucumber.component.thresholds;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+public class ThresholdsSteps {
+}
