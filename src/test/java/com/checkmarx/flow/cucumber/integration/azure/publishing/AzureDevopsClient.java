c8fa3d34d54d60ebdf62bcf424fc1d734363622a
AlexeyK <alexey.kononov@checkmarx.com>
2020-03-03 12:53:26 +0200

    Implemented counting ADO issues.

--- src/test/java/com/checkmarx/flow/cucumber/integration/azure/publishing/AzureDevopsClient.java
+++ src/test/java/com/checkmarx/flow/cucumber/integration/azure/publishing/AzureDevopsClient.java
@@ -13,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.annotation.Nullable;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
@@ -50,38 +51,64 @@ public class AzureDevopsClient {
         this.adoProperties = adoProperties;
     }
 
-    public void ensureProjectExists(String projectName) {
+    public void ensureProjectExists(String projectName) throws IOException {
         if (!projectExists(projectName)) {
             String operationId = queueProjectCreation(projectName);
             waitUntilProjectIsCreated(operationId);
         }
     }
 
-    private boolean projectExists(String projectName) {
-        HttpHeaders headers = getHeaders();
-        HttpEntity<?> entity = new HttpEntity<>(headers);
+    public int getIssueCount(String projectName) throws IOException {
+        ObjectNode requestBody = objectMapper.createObjectNode();
+
+        // WIQL language is read-only, so potential parameter injection shouldn't do any harm.
+        String wiqlQuery = String.format("Select System.Id From WorkItems Where System.TeamProject = '%s'", projectName);
+
+        requestBody.put("query", wiqlQuery);
+        HttpEntity<ObjectNode> request = getRequestEntity(requestBody);
+
+        String url = getResourceUrl("wit/wiql", null);
+
+        ResponseEntity<ObjectNode> response = restClient.exchange(url, HttpMethod.POST, request, ObjectNode.class);
+
+        ObjectNode responseBody = extractBody(response);
+        return responseBody.get("workItems").size();
+    }
+
+    private ObjectNode extractBody(HttpEntity<ObjectNode> response) throws IOException {
+        ObjectNode body = response.getBody();
+        if (body != null) {
+            return body;
+        } else {
+            throw new IOException("Response body is null.");
+        }
+    }
+
+    private boolean projectExists(String projectName) throws IOException {
+        HttpEntity<?> request = getRequestEntity(null);
 
         String url = getResourceUrl("projects", null);
-        ResponseEntity<ObjectNode> response = restClient.exchange(url, HttpMethod.GET, entity, ObjectNode.class);
-        return StreamSupport.stream(response.getBody().get("value").spliterator(), false)
+        ResponseEntity<ObjectNode> response = restClient.exchange(url, HttpMethod.GET, request, ObjectNode.class);
+        ObjectNode body = extractBody(response);
+
+        return StreamSupport.stream(body.get("value").spliterator(), false)
                 .anyMatch(withSame(projectName));
     }
 
-    private String queueProjectCreation(String projectName) {
-        ObjectNode body = null;
+    private String queueProjectCreation(String projectName) throws IOException {
+        ObjectNode body;
         try {
             body = (ObjectNode) objectMapper.readTree(PROJECT_CREATION_REQUEST_TEMPLATE);
             body.put("name", projectName);
         } catch (JsonProcessingException e) {
             log.error("JSON parse error.", e);
+            throw new IOException(e);
         }
 
-        HttpHeaders headers = getHeaders();
-        HttpEntity<?> entity = new HttpEntity<>(body, headers);
-
         String url = getResourceUrl("projects", null);
+        HttpEntity<?> entity = getRequestEntity(body);
         ResponseEntity<ObjectNode> response = restClient.exchange(url, HttpMethod.POST, entity, ObjectNode.class);
-        String operationId = response.getBody().get("id").textValue();
+        String operationId = extractBody(response).get("id").textValue();
         log.info("Queued project creation: operation ID {}.", operationId);
         return operationId;
     }
@@ -93,19 +120,19 @@ public class AzureDevopsClient {
                 .until(() -> projectIsCreated(operationId));
     }
 
-    private boolean projectIsCreated(String operationId) {
+    private boolean projectIsCreated(String operationId) throws IOException {
         String url = getResourceUrl("operations", operationId);
-        HttpHeaders headers = getHeaders();
-        HttpEntity<?> entity = new HttpEntity<>(headers);
+        HttpEntity<Object> request = getRequestEntity(null);
 
-        ResponseEntity<ObjectNode> response = restClient.exchange(url, HttpMethod.GET, entity, ObjectNode.class);
+        ResponseEntity<ObjectNode> response = restClient.exchange(url, HttpMethod.GET, request, ObjectNode.class);
 
-        String status = response.getBody().get("status").textValue();
+        String status = extractBody(response).get("status").textValue();
         log.info("Status: {}.", status);
+
         return status.equals("succeeded");
     }
 
-    private HttpHeaders getHeaders() {
+    private <T> HttpEntity<T> getRequestEntity(T body) {
         HttpHeaders headers = new HttpHeaders();
 
         List<MediaType> mediaTypes = new ArrayList<>();
@@ -115,12 +142,15 @@ public class AzureDevopsClient {
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         headers.setBasicAuth("", adoProperties.getToken());
-        return headers;
+
+        return new HttpEntity<>(body, headers);
     }
 
     private String getResourceUrl(String resourceName, @Nullable String id) {
         return UriComponentsBuilder.fromHttpUrl(adoProperties.getUrl())
-                .pathSegment("_apis", resourceName, id)
+                .pathSegment("_apis")
+                .path(resourceName)
+                .pathSegment(id)
                 .queryParam("api-version", adoProperties.getApiVersion())
                 .toUriString();
 
--- src/test/java/com/checkmarx/flow/cucumber/integration/azure/publishing/PublishingSteps.java
+++ src/test/java/com/checkmarx/flow/cucumber/integration/azure/publishing/PublishingSteps.java
@@ -3,14 +3,20 @@ package com.checkmarx.flow.cucumber.integration.azure.publishing;
 import com.checkmarx.flow.CxFlowApplication;
 import com.checkmarx.flow.config.ADOProperties;
 import com.checkmarx.flow.config.FlowProperties;
+import com.checkmarx.flow.cucumber.common.utils.TestUtils;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+
 @SpringBootTest(classes = {CxFlowApplication.class})
 public class PublishingSteps {
-    private static final String PROJECT_NAME = "CxFlowTest3";
+    private static final String PROPERTIES_FILE_PATH = "cucumber/features/integrationTests/azure/publishing.properties";
 
     @Autowired
     private FlowProperties flowProperties;
@@ -20,16 +26,23 @@ public class PublishingSteps {
 
     private AzureDevopsClient adoClient;
 
+    private String projectName;
+
     @Before
-    public void prepareEnvironment() {
+    public void prepareEnvironment() throws IOException {
+        Properties testProperties = TestUtils.getPropertiesFromResource(PROPERTIES_FILE_PATH);
+
+        projectName = testProperties.getProperty("projectName");
+
         flowProperties.setBugTracker("Azure");
-        adoProperties.setUrl("https://dev.azure.com/myorganization");
 
         adoClient = new AzureDevopsClient(adoProperties);
-        adoClient.ensureProjectExists(PROJECT_NAME);
+        adoClient.ensureProjectExists(projectName);
     }
 
     @Given("Azure DevOps doesn't contain any issues")
-    public void azureDevOpsDoesnTContainAnyIssues() {
+    public void azureDevOpsDoesnTContainAnyIssues() throws IOException {
+        int actualIssueCount = adoClient.getIssueCount(projectName);
+        assertEquals("Unexpected issue count.", 0, actualIssueCount);
     }
 }
