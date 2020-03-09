8ada510790b50590049b8975150c7b52bc7a97e7
AlexeyK <alexey.kononov@checkmarx.com>
2020-03-09 16:00:29 +0200

    Removed duplicate method.

--- src/test/java/com/checkmarx/flow/cucumber/common/utils/TestUtils.java
+++ src/test/java/com/checkmarx/flow/cucumber/common/utils/TestUtils.java
@@ -71,11 +71,4 @@ public class TestUtils {
     public static ConfigurableApplicationContext runCxFlowAsService() {
         return SpringApplication.run(CxFlowApplication.class, "--web");
     }
-
-    public static Properties getPropertiesFromResource(String path) throws IOException {
-        File file = ResourceUtils.getFile("classpath:" + path);
-        Properties result = new Properties();
-        result.load(Files.newInputStream(file.toPath()));
-        return result;
-    }
 }
