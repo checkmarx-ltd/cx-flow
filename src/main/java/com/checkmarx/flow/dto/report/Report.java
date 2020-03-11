066d8d4db79dfae3c1a92805592386677694579d
orlyk <orly.kilidgan@checkmarx.com>
2020-03-11 11:57:40 +0200

    pull request analytics

--- src/main/java/com/checkmarx/flow/dto/report/Report.java
+++ src/main/java/com/checkmarx/flow/dto/report/Report.java
@@ -2,37 +2,39 @@ package com.checkmarx.flow.dto.report;
 
 import com.checkmarx.flow.dto.ScanRequest;
 import com.checkmarx.flow.exception.MachinaException;
+import groovy.util.logging.Slf4j;
 import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import static net.logstash.logback.marker.Markers.*;
 
 @Data
-public abstract class Writable {
+@Slf4j
+public abstract class Report {
 
     public static final Logger jsonlogger = LoggerFactory.getLogger("jsonLogger");
 
-    public static final Logger log = org.slf4j.LoggerFactory.getLogger(Writable.class);
+    //public static final Logger log = org.slf4j.LoggerFactory.getLogger(Writable.class);
     public static final String SAST = "SAST";
-    private static final String OSA = "OSA";
+    public static final String OSA = "OSA";
 
     protected String scanId;
     protected String repoUrl;
     protected String scanType;
     
-    public Writable(String scanId, ScanRequest request) {
+    public Report(String scanId, ScanRequest request) {
         this.scanId = scanId;
         this.repoUrl = request.getRepoUrl();
         scanType = OSA;
     }
 
-    public Writable(Integer scanId, ScanRequest request) {
+    public Report(Integer scanId, ScanRequest request) {
         this.scanId = scanId.toString();
         this.repoUrl = request.getRepoUrl();
         scanType = SAST;
     }
     
-    public void write() throws MachinaException {
+    public void log() {
         jsonlogger.info(append(_getOperation(), this), "");
     }
     
