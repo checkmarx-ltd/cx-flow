2089caf8351fd901a5b64087d60ee924b293735c
olgakil <33619803+olgakil@users.noreply.github.com>
2020-03-29 10:29:25 +0300

    Orly logger jira tickets report (#80)
    * Get Request - add vulnerabilities
    Also - add rolling appender dynamic size
    
    * Add Jira Report Support
    
    * fix skip
    
    * Change report - log objects
    
    * Change report - log objects
    
    * Change report - log objects

--- src/main/java/com/checkmarx/flow/dto/report/PullRequestReport.java
+++ src/main/java/com/checkmarx/flow/dto/report/PullRequestReport.java
@@ -3,6 +3,7 @@ package com.checkmarx.flow.dto.report;
 import com.checkmarx.flow.config.FindingSeverity;
 import com.checkmarx.flow.dto.ScanDetails;
 import com.checkmarx.flow.dto.ScanRequest;
+import lombok.Getter;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,19 +12,20 @@ import static net.logstash.logback.marker.Markers.append;
 import static net.logstash.logback.marker.Markers.appendEntries;
 
 
+@Getter
 public class PullRequestReport extends AnalyticsReport {
 
     public static final String OPERATION = "Pull Request";
 
     private String pullRequestStatus;
 
-    private Map<FindingSeverity, Integer> findingsPerSeverity = null;
+    private Map<FindingSeverity, Integer> findingsMap = null;
     private Map<FindingSeverity, Integer> thresholds = null;
 
-    
+
     public PullRequestReport(ScanDetails scanDetails, ScanRequest request) {
         super(scanDetails.getScanId(), request);
-        
+
         repoUrl = request.getRepoUrl();
         if(scanDetails.isOsaScan()){
             scanId = scanDetails.getOsaScanId();
@@ -32,10 +34,6 @@ public class PullRequestReport extends AnalyticsReport {
     }
 
 
-    public String getPullRequestStatus() {
-        return pullRequestStatus;
-    }
-    
     public void setPullRequestStatus(String status){
         this.pullRequestStatus = status;
     }
@@ -47,14 +45,8 @@ public class PullRequestReport extends AnalyticsReport {
         return OPERATION;
     }
 
-    public void setFindingsPerSeverity(Iterable<Map.Entry<FindingSeverity, Integer>> findingsPerSeverity) {
-
-        Map<FindingSeverity, Integer> findingsMap = new HashMap<>();
-        
-        for (Map.Entry<FindingSeverity, Integer> entry: findingsPerSeverity) {
-            findingsMap.put(entry.getKey(),entry.getValue() );
-        }
-        this.findingsPerSeverity = findingsMap;
+    public void setFindingsPerSeverity(Map<FindingSeverity, Integer> findingsMap) {
+        this.findingsMap = findingsMap;
     }
 
     public void setThresholds(Map<FindingSeverity, Integer> thresholds) {
@@ -65,12 +57,5 @@ public class PullRequestReport extends AnalyticsReport {
     public void log()  {
         jsonlogger.info(append(_getOperation() , this), "");
 
-        if (findingsPerSeverity != null) {
-            jsonlogger.info(appendEntries(findingsPerSeverity), "");
-        }
-
-        if (thresholds != null) {
-            jsonlogger.info(appendEntries(thresholds), "");
-        }
     }
 }
