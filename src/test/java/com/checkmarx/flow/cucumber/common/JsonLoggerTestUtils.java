90406570624f4a39decae7e632722751b5eb610a
orlyk <orly.kilidgan@checkmarx.com>
2020-03-30 12:56:26 +0300

    change JsonLoggerTestUtils - reuse existing method

--- src/test/java/com/checkmarx/flow/cucumber/common/JsonLoggerTestUtils.java
+++ src/test/java/com/checkmarx/flow/cucumber/common/JsonLoggerTestUtils.java
@@ -27,35 +27,15 @@ public class JsonLoggerTestUtils {
         logAbsolutePath = System.getProperty(logPath) + File.separator + "CxFlowReport.json";
     }
 
-    public AnalyticsReport getOperationNode(String operation , Class reportClass) throws CheckmarxException {
+    public AnalyticsReport getReportNode(String nodeName , Class reportClass) throws CheckmarxException {
+        
+        ObjectMapper objectMapper = new ObjectMapper(); 
+        JsonNode node = getReportNode(nodeName, objectMapper);
 
+        objectMapper.configure(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS, false);
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        try (
-
-                FileInputStream inputStream = new FileInputStream(logAbsolutePath);
-                BufferedReader streamReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-        ){
-            boolean moreLines = true;
-            String scanRequest = streamReader.readLine();
-            String nextScanRequest = null;
-            while (moreLines) {
-                nextScanRequest = streamReader.readLine();
-                if (nextScanRequest != null) {
-                    scanRequest = nextScanRequest;
-                } else {
-                    moreLines = false;
-                }
-            }
-
-            String node = objectMapper.readTree(scanRequest).get(operation).toString();
-
-            objectMapper.configure(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS, false);
-            AnalyticsReport reportObject =  (AnalyticsReport)objectMapper.readValue(node, reportClass);
-
-
-            return reportObject;
-
+        try {
+            return (AnalyticsReport)objectMapper.readValue(node.toString(), reportClass);
         } catch (IOException e) {
             throw new CheckmarxException(e.getMessage());
         }
@@ -63,11 +43,12 @@ public class JsonLoggerTestUtils {
     }
 
 
+    public JsonNode getReportNode(String nodeName) throws CheckmarxException {
+        return getReportNode(nodeName, new ObjectMapper());
+    }
 
-
-    public JsonNode getOperationNode(String operation) throws CheckmarxException {
-
-        ObjectMapper objectMapper = new ObjectMapper();
+    private JsonNode getReportNode(String nodeName, ObjectMapper objectMapper) throws CheckmarxException {
+        
          try (
 
             FileInputStream inputStream = new FileInputStream(logAbsolutePath);
