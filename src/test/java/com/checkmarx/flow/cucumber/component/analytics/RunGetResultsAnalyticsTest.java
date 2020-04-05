6d045effea51f5846c012b19e226c11273a35493
udiraz2 <54795363+udiraz2@users.noreply.github.com>
2020-04-05 15:37:22 +0300

    Analytics tests for get results operation (#98)
    * Added tests (component) for analytics when doing get results operation.
    
    * Fixed GetRestultsReport/ScanResultsReport.

---
+++ src/test/java/com/checkmarx/flow/cucumber/component/analytics/RunGetResultsAnalyticsTest.java
@@ -0,0 +1,16 @@
+package com.checkmarx.flow.cucumber.component.analytics;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(Cucumber.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@CucumberOptions(plugin = { "pretty", "summary", "html:build/cucumber/component/api", "json:build/cucumber/component/api/cucumber.json" },
+        features = "classpath:cucumber/features/componentTests/analytics-get-results.feature",
+        glue = { "com.checkmarx.flow.cucumber.common.steps", "com.checkmarx.flow.cucumber.component.analytics" },
+        tags = "@Component and not @Skip")
+
+public class RunGetResultsAnalyticsTest {
+}
