361c4a3baf8082f66c347ab46fa0f0021362ea75
Tala <tal.akalo@checkmarx.com>
2020-04-06 14:04:30 +0300

    integration tests added

--- src/test/java/com/checkmarx/flow/cucumber/integration/negative_tests/RunJiraNegativeSteps.java
+++ src/test/java/com/checkmarx/flow/cucumber/integration/negative_tests/RunJiraNegativeSteps.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = { "pretty", "summary", "html:build/cucumber/integration/negative_tests", "json:build/cucumber/integration/negative_tests/cucumber.json" },
         features = "src/test/resources/cucumber/features/integrationTests/jira/publish-processing.feature",
-        glue = { "com.checkmarx.flow.cucumber.integration.negative_tests"},
+        glue = {"com.checkmarx.flow.cucumber.common.steps", "com.checkmarx.flow.cucumber.integration.negative_tests"},
         tags = "@Integration and @Negative_test and @Error_Handling and not @Skip")
 public class RunJiraNegativeSteps {
 }
