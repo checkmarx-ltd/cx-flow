a15c9667f0027147b1d62718aeefbaf9b925802f
orlyk <orly.kilidgan@checkmarx.com>
2020-04-20 17:12:43 +0300

    remove common steps from test runner

--- src/test/java/com/checkmarx/flow/cucumber/integration/cxconfig/CxConfigTestRunner.java
+++ src/test/java/com/checkmarx/flow/cucumber/integration/cxconfig/CxConfigTestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = { "pretty", "summary", "html:build/cucumber/integration/cxconfig"},
         features = "src/test/resources/cucumber/features/integrationTests/cxconfig.feature",
-        glue = { "com.checkmarx.flow.cucumber.common.steps", "com.checkmarx.flow.cucumber.integration.cxconfig" },
+        glue = { "com.checkmarx.flow.cucumber.integration.cxconfig" },
         tags = "@CxConfigFeature and not @Skip")
 public class CxConfigTestRunner {
 }
