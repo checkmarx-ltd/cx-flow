c392c438f5f7ca0450cfc5ef1083d7fd97bbff0b
cxflowtestuser <60353024+cxflowtestuser@users.noreply.github.com>
2020-05-06 18:02:02 +0300

    Pr generic e2e (#125)
    * create a generic e2e test
    
    * implemented GitHub to Jira
    
    * implemented ADO to Jira
    
    
    Co-authored-by: Ofer Skolsky <Ofer.Skolsky@checkmarx.com>

---
+++ src/main/java/com/checkmarx/flow/dto/azure/PublisherInputs.java
@@ -0,0 +1,14 @@
+package com.checkmarx.flow.dto.azure;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PublisherInputs {
+    private String projectId;
+}
--- src/test/java/com/checkmarx/flow/cucumber/integration/jira/manualconfig/ManualConfigRunner.java
+++ src/test/java/com/checkmarx/flow/cucumber/integration/jira/manualconfig/ManualConfigRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = { "pretty", "summary", "html:build/cucumber/integration/scan"},
         features = "classpath:cucumber/features/integrationTests/jira/jira-manual-config.feature",
-        glue = { "com.checkmarx.flow.cucumber.common.steps", "com.checkmarx.flow.cucumber.integration.jira.manualconfig" },
+        glue = { "com.checkmarx.flow.cucumber.integration.jira.manualconfig" },
         tags = "@Integration and not @Skip")
 public class ManualConfigRunner {
 }
