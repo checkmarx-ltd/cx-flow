c9e9ca9dadb37002f6fc15c4efc0e3c4001d8c12
Ken McDonald <ken.mcdonald@checkmarx.com>
2020-05-18 01:33:08 -0400

    GitLabDashboard Support (#181)
    * Modified: GetLabIssueTracker::getProjectDetails(String repoName) no longer uses the 'old' V3 GitLab API which required namespace when searching for project. The new version uses the V4 API which simply searches for the project by name. The old API no longer worked correctly
    
    Removed unused 'second' version of GetLabIssueTracker::getProjectDetails(Integer projectId).
    
    GetLabeService:getConfigAsCode() no longer throws an exception when config-as-code file isn't found. Now, it simply ignores it and moves on; previously, it threw the exception and then moved on anyway.
    (cherry picked from commit ffea67d180a77525766f05d71dd071b8829d9700)
    
    * Adding file path as option to RepoProperties to accomodate CI/CD file outputs associated with any of Repositories
    
    (cherry picked from commit f189f9c11859e85d1119ad18d9ef9e55df95c7d7)
    
    * Initial stub for GitLab Security Dashboard integration.
    
    (cherry picked from commit 390a43cf3703ace63d284f05c0e67b8fe5a3e8f6)
    
    * GitLab Security Dashboard JSON Report Logic
    
    (cherry picked from commit 9369b633df3e26902a315514427b3bf9ca343661)
    
    * Changes after debugging GitLab CI Pipeline with Security Dashboards.
    
    (cherry picked from commit 982a3275c651750e46009abd1152525b702da9a8)
    
    * Update to ensure id of issue is unique in GitLab Dashboard.
    
    (cherry picked from commit 0dea8063f458c42152a847c4f314e3cbca5b76db)
    
    * Update SDK to version 0.4.15 to include OSA parsing bug fixes reported by customer
    
    Co-authored-by: Jeff Armstrong <jeff.armstrong@checkmarx.com>

---
+++ src/main/java/com/checkmarx/flow/custom/GitLabSecurityDasbhoard.java
@@ -0,0 +1,259 @@
+package com.checkmarx.flow.custom;
+
+import com.checkmarx.flow.config.FlowProperties;
+import com.checkmarx.flow.config.GitLabProperties;
+import com.checkmarx.flow.dto.Issue;
+import com.checkmarx.flow.dto.ScanRequest;
+import com.checkmarx.flow.exception.MachinaException;
+import com.checkmarx.flow.utils.ScanUtils;
+import com.checkmarx.sdk.dto.ScanResults;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Builder;
+import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service("GitLabDashboard")
+public class GitLabSecurityDasbhoard implements IssueTracker {
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(GitLabSecurityDasbhoard.class);
+    private final GitLabProperties properties;
+    private final FlowProperties flowProperties;
+    private final String ISSUE_FORMAT = "%s @ %s : %d";
+
+    public GitLabSecurityDasbhoard(GitLabProperties properties, FlowProperties flowProperties) {
+        this.properties = properties;
+        this.flowProperties = flowProperties;
+    }
+
+    @Override
+    public void init(ScanRequest request, ScanResults results) throws MachinaException {
+        String filename = properties.getFilePath();
+        filename = ScanUtils.getFilename(request, filename);
+        request.setFilename(filename);
+        log.info("Creating file {}", filename);
+        try {
+            Files.deleteIfExists(Paths.get(filename));
+            Files.createFile(Paths.get(filename));
+        } catch (IOException e) {
+            log.error("Issue deleting existing file or writing initial {}", filename, e);
+        }
+    }
+
+    @Override
+    public void complete(ScanRequest request, ScanResults results) throws MachinaException {
+        log.info("Finalizing Dashboard output");
+        List<Vulnerability> vulns = new ArrayList<>();
+        Scanner scanner = Scanner.builder().build();
+        results.getXIssues().forEach( issue -> {
+            issue.getDetails().forEach( (k,v) -> {
+                Vulnerability vuln = Vulnerability.builder()
+                        .category("sast")
+                        .id(issue.getVulnerability().concat(":").concat(issue.getFilename()).concat(":").concat(k.toString()))
+                        .cve(issue.getVulnerability().concat(":").concat(issue.getFilename()).concat(":").concat(k.toString()))
+                        .name(issue.getVulnerability())
+                        .message(String.format(ISSUE_FORMAT, issue.getVulnerability(), issue.getFilename(), k))
+                        .description(issue.getVulnerability())
+                        .severity(issue.getSeverity())
+                        .confidence(issue.getSeverity())
+                        .solution(issue.getLink())
+                        .scanner(scanner)
+                        .identifiers(getIdentifiers(issue))
+                        .location(
+                                Location.builder()
+                                        .file(issue.getFilename())
+                                        .startLine(k)
+                                        .endLine(k)
+                                        .build()
+                        )
+                        .build();
+                vulns.add(vuln);
+            });
+        });
+        SecurityDashboard report  = SecurityDashboard.builder()
+                .vulnerabilities(vulns)
+                .build();
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+            mapper.writeValue(new File(request.getFilename()), report);
+        } catch (IOException e) {
+            log.error("Issue occurred while writing file {}", request.getFilename(), e);
+            throw new MachinaException();
+        }
+    }
+
+    private List<Identifier> getIdentifiers(ScanResults.XIssue issue){
+        List<Identifier> identifiers = new ArrayList<>();
+        identifiers.add(
+                Identifier.builder()
+                        .type("checkmarx_finding")
+                        .name("Checkmarx-".concat(issue.getVulnerability()))
+                        .value(issue.getVulnerability())
+                        .url(issue.getLink())
+                        .build()
+        );
+        identifiers.add(
+                Identifier.builder()
+                        .type("cwe")
+                        .name("CWE-".concat(issue.getCwe()))
+                        .value(issue.getCwe())
+                        .url(String.format(flowProperties.getMitreUrl(), issue.getCwe()))
+                        .build()
+        );
+        return identifiers;
+    }
+
+    @Override
+    public String getFalsePositiveLabel() throws MachinaException {
+        return null;
+    }
+
+    @Override
+    public List<Issue> getIssues(ScanRequest request) throws MachinaException {
+        return null;
+    }
+
+    @Override
+    public Issue createIssue(ScanResults.XIssue issue, ScanRequest request) throws MachinaException {
+        return null;
+    }
+
+    @Override
+    public void closeIssue(Issue issue, ScanRequest request) throws MachinaException {
+
+    }
+
+    @Override
+    public Issue updateIssue(Issue issue, ScanResults.XIssue resultIssue, ScanRequest request) throws MachinaException {
+        return null;
+    }
+
+    @Override
+    public String getIssueKey(Issue issue, ScanRequest request) {
+        return issue.getId();
+    }
+
+    @Override
+    public String getXIssueKey(ScanResults.XIssue issue, ScanRequest request) {
+        return issue.getFilename();
+    }
+
+    @Override
+    public boolean isIssueClosed(Issue issue, ScanRequest request) {
+        return false;
+    }
+
+    @Override
+    public boolean isIssueOpened(Issue issue, ScanRequest request) {
+        return false;
+    }
+
+    @Data
+    @Builder
+    public static class SecurityDashboard {
+        @JsonProperty("version")
+        @Builder.Default
+        public Double version = 2.0;
+        @JsonProperty("vulnerabilities")
+        public List<Vulnerability> vulnerabilities;
+        @JsonProperty("remediations")
+        public List<String> remediations;
+    }
+
+    @Data
+    @Builder
+    public static class Vulnerability {
+        @JsonProperty("id")
+        public String id;
+        @JsonProperty("cve")
+        public String cve;
+        @JsonProperty("category")
+        @Builder.Default
+        public String category = "sast";
+        @JsonProperty("name")
+        public String name;
+        @JsonProperty("message")
+        public String message;
+        @JsonProperty("description")
+        public String description;
+        @JsonProperty("location")
+        public Location location;
+        @JsonProperty("severity")
+        public String severity;
+        @JsonProperty("confidence")
+        public String confidence;
+        @JsonProperty("solution")
+        public String solution;
+        @JsonProperty("scanner")
+        public Scanner scanner;
+        @JsonProperty("identifiers")
+        public List<Identifier> identifiers;
+    }
+
+    @Data
+    @Builder
+    public static class Scanner {
+        @JsonProperty("id")
+        @Builder.Default
+        public String id = "Checkmarx";
+        @JsonProperty("name")
+        @Builder.Default
+        public String name = "Checkmarx";
+    }
+
+    @Data
+    @Builder
+    public static class Location {
+        @JsonProperty("file")
+        public String file;
+        @JsonProperty("start_line")
+        public Integer startLine;
+        @JsonProperty("end_line")
+        public Integer endLine;
+        @Builder.Default
+        @JsonProperty("class")
+        public String clazz = "N/A";
+        @Builder.Default
+        @JsonProperty("method")
+        public String method = "N/A";
+        @JsonProperty("dependency")
+        public Dependency dependency;
+    }
+
+    @Data
+    @Builder
+    public static class Dependency {
+        @JsonProperty("package")
+        public Object pkg;
+    }
+
+    @Data
+    @Builder
+    public static class Identifier {
+        @JsonProperty("type")
+        public String type;
+        @JsonProperty("name")
+        public String name;
+        @JsonProperty("value")
+        public String value;
+        @JsonProperty("url")
+        public String url;
+    }
+}
