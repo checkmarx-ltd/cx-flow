0056524b7ad78536fb2982f17958561e553e444b
AlexeyK <alexey.kononov@checkmarx.com>
2020-05-20 12:52:53 +0300

    Fixed a compilation error. Test improvement.

--- src/test/java/com/checkmarx/flow/cucumber/integration/sast/config/OverwritingProjectConfigSteps.java
+++ src/test/java/com/checkmarx/flow/cucumber/integration/sast/config/OverwritingProjectConfigSteps.java
@@ -7,6 +7,7 @@ import com.checkmarx.flow.controller.GitHubController;
 import com.checkmarx.flow.service.FlowService;
 import com.checkmarx.flow.service.GitHubService;
 import com.checkmarx.flow.service.HelperService;
+import com.checkmarx.flow.service.SastScanner;
 import com.checkmarx.flow.utils.github.GitHubTestUtils;
 import com.checkmarx.sdk.config.CxProperties;
 import com.checkmarx.sdk.dto.cx.CxScanParams;
@@ -28,7 +29,9 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -40,28 +43,30 @@ import static org.mockito.Mockito.doAnswer;
 @Slf4j
 @SpringBootTest(classes = {CxFlowApplication.class, GitHubTestUtils.class})
 public class OverwritingProjectConfigSteps {
+    public static final Duration SCAN_CREATION_TIMEOUT = Duration.ofSeconds(30);
     private static final ObjectMapper jsonMapper = new ObjectMapper();
 
     @SpyBean
     private final CxClient cxClientSpy;
 
     private final CxProperties cxProperties;
-    private final GitHubTestUtils gitHubTestUtils;
-
     private final FlowProperties flowProperties;
     private final GitHubProperties gitHubProperties;
+    private final GitHubTestUtils gitHubTestUtils;
     private final HelperService helperService;
-
     private final GitHubService gitHubService;
     private final FlowService flowService;
+    private final SastScanner sastScanner;
 
-    private Integer initialPresetId;
-    private Integer initialConfigId;
     private Integer projectId;
-    private Integer scanId;
+    private Integer interceptedScanId;
+
+    // Name to ID mappings.
+    private final Map<String, Integer> presetMapping = new HashMap<>();
+    private final Map<String, Integer> configMapping = new HashMap<>();
 
     @Before()
-    public void disableMultiTenant() {
+    public void ensureCorrectTeam() {
         // Make sure we use the same team in the test and during scan (without the namespace postfix).
         // Otherwise we'll get 2 projects with the same name but in different teams.
         cxProperties.setMultiTenant(false);
@@ -70,24 +75,31 @@ public class OverwritingProjectConfigSteps {
     @Given("{string} project exists in SAST")
     public void projectExistsInSAST(String projectName) throws CheckmarxException {
         this.projectId = ensureProjectExists(projectName);
+        enableScanIdInterception();
     }
 
     @And("all of {string}, {string}, {string}, {string} exist in SAST")
-    public void allOfExistInSAST(String initialPreset, String initialConfig, String newPreset, String newConfig) throws CheckmarxException {
-        initialPresetId = cxClientSpy.getPresetId(initialPreset);
-        initialConfigId = cxClientSpy.getScanConfiguration(initialConfig);
-        Integer newPresetId = cxClientSpy.getPresetId(newPreset);
-        Integer newConfigId = cxClientSpy.getScanConfiguration(newConfig);
+    public void allOfExistInSAST(String initialPreset, String initialConfig, String globalPreset, String globalConfig) throws CheckmarxException {
+        Integer initialPresetId = cxClientSpy.getPresetId(initialPreset);
+        Integer globalPresetId = cxClientSpy.getPresetId(globalPreset);
+        Integer initialConfigId = cxClientSpy.getScanConfiguration(initialConfig);
+        Integer globalConfigId = cxClientSpy.getScanConfiguration(globalConfig);
 
-        List<Integer> ids = Arrays.asList(initialPresetId, initialConfigId, newPresetId, newConfigId);
+        List<Integer> ids = Arrays.asList(initialPresetId, globalPresetId, initialConfigId, globalConfigId);
         boolean allEntitiesExist = ids.stream().allMatch(id -> id > 0);
 
         assertTrue(allEntitiesExist, () -> String.format("Some of the entities don't exist in SAST: %s.", ids));
+
+        presetMapping.put(initialPreset, initialPresetId);
+        presetMapping.put(globalPreset, globalPresetId);
+
+        configMapping.put(initialConfig, initialConfigId);
+        configMapping.put(globalConfig, globalConfigId);
     }
 
     @And("project has the {string} preset and the {string} scan configuration")
     public void projectPresetIs(String preset, String config) {
-        cxClientSpy.createScanSetting(projectId, initialPresetId, initialConfigId);
+        cxClientSpy.createScanSetting(projectId, presetMapping.get(preset), configMapping.get(config));
     }
 
     @And("CxFlow config has the {string} preset and the {string} scan configuration")
@@ -102,14 +114,10 @@ public class OverwritingProjectConfigSteps {
     }
 
     @When("GitHub notifies CxFlow about a pull request for the {string} project")
-    public void githubNotifiesCxFlowAboutAPullRequest(String projectName) throws CheckmarxException {
-        doAnswer(invocation -> {
-            scanId = (Integer) invocation.callRealMethod();
-            return scanId;
-        }).when(cxClientSpy).createScan(any(CxScanParams.class), anyString());
+    public void githubNotifiesCxFlowAboutAPullRequest(String projectName) {
 
         GitHubController gitHubController = new GitHubController(gitHubProperties, flowProperties, cxProperties,
-                null, flowService, helperService, gitHubService);
+                null, flowService, helperService, gitHubService, sastScanner);
 
         gitHubTestUtils.callController(gitHubController, GitHubTestUtils.EventType.PULL_REQUEST, projectName);
     }
@@ -118,22 +126,28 @@ public class OverwritingProjectConfigSteps {
     public void cxflowStartsASASTScan() {
         log.info("Waiting until scan is created.");
         Awaitility.await()
-                .atMost(Duration.ofMinutes(10))
-                .until(() -> scanId != null);
-
-        log.info("Scan is created, ID {}.", scanId);
+                .atMost(SCAN_CREATION_TIMEOUT)
+                .until(() -> interceptedScanId != null);
+        log.info("Scan is created, ID: {}.", interceptedScanId);
     }
 
     @Then("project preset is still {string} and scan configuration is {string}")
-    public void projectPresetIsStill(String expectedPresetName, String expectedConfigName) throws JsonProcessingException {
+    public void projectPresetIsStill(String expectedPreset, String expectedConfigName) throws JsonProcessingException {
         String configJson = cxClientSpy.getScanSetting(projectId);
         JsonNode config = jsonMapper.readTree(configJson);
 
         int currentPresetId = config.at("/preset/id").asInt();
-        assertEquals(initialPresetId, currentPresetId, "Preset has changed after starting a scan.");
+        assertEquals(presetMapping.get(expectedPreset), currentPresetId, "Preset has changed after starting a scan.");
 
         int currentConfigId = config.at("/engineConfiguration/id").asInt();
-        assertEquals(initialConfigId, currentConfigId, "Configuration has changed after starting a scan.");
+        assertEquals(configMapping.get(expectedConfigName), currentConfigId, "Configuration has changed after starting a scan.");
+    }
+
+    private void enableScanIdInterception() throws CheckmarxException {
+        doAnswer(invocation -> {
+            interceptedScanId = (Integer) invocation.callRealMethod();
+            return interceptedScanId;
+        }).when(cxClientSpy).createScan(any(CxScanParams.class), anyString());
     }
 
     private Integer ensureProjectExists(String projectName) throws CheckmarxException {
