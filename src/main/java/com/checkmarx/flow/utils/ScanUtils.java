77f15eb3196d6418d29dbedba1b27bbd6b1586a1
Ofer Skolsky <Ofer.Skolsky@checkmarx.com>
2020-06-07 12:54:34 +0300

    sonar: improve cognitivity.

--- src/main/java/com/checkmarx/flow/utils/ScanUtils.java
+++ src/main/java/com/checkmarx/flow/utils/ScanUtils.java
@@ -38,6 +38,7 @@ import com.checkmarx.flow.dto.Field;
 import com.checkmarx.flow.dto.FlowOverride;
 import com.checkmarx.flow.dto.RepoIssue;
 import com.checkmarx.flow.dto.ScanRequest;
+import com.checkmarx.flow.dto.BugTracker.BugTrackerBuilder;
 import com.checkmarx.flow.exception.MachinaRuntimeException;
 import com.checkmarx.sdk.config.Constants;
 import com.checkmarx.sdk.dto.CxConfig;
@@ -77,6 +78,9 @@ public class ScanUtils {
     public static final String SEVERITY = "Severity: ";
     public static final String DIV = "</div>";
 
+    private ScanUtils() {
+        // this is to hide the public constractor
+    }
     /**
      * Function used to determine if file extension of full filename is preset in list
      *
@@ -249,127 +253,126 @@ public class ScanUtils {
         if(override == null || request == null || !override.getActive()){
             return request;
         }
-        if (!ScanUtils.empty(override.getProject())) {
+        Optional.ofNullable(override.getProject())
+        .filter(ne -> !empty(ne))
+        .ifPresent(p -> {
             /*Replace ${repo} and ${branch}  with the actual reponame and branch - then strip out non-alphanumeric (-_ are allowed)*/
-            String project = override.getProject();
-            project = project.replaceAll("\\$\\{repo}", request.getRepoName());
-            project = project.replaceAll("\\$\\{branch}", request.getBranch());
-            project = project.replaceAll("[^a-zA-Z0-9-_.]+","-");
+            String project = p.replace("${repo}", request.getRepoName())
+            .replace("${branch}", request.getBranch())
+            .replaceAll("[^a-zA-Z0-9-_.]+","-");
             request.setProject(project);
             overridePropertiesMap.put("project", project);
-        }
-        if (!ScanUtils.empty(override.getTeam())) {
-            request.setTeam(override.getTeam());
-            overridePropertiesMap.put("team", override.getTeam());
-        }
-        if(override.getSast() != null) {
-            if (override.getSast().getIncremental() != null) {
-                request.setIncremental(override.getSast().getIncremental());
-                overridePropertiesMap.put("incremental", override.getSast().getIncremental().toString());
-            }
+        });
+        Optional.ofNullable(override.getTeam())
+        .filter(ne -> !empty(ne))
+        .ifPresent(t -> {
+            request.setTeam(t);
+            overridePropertiesMap.put("team", t);
+        });
+        Optional.ofNullable(override.getSast()).ifPresent(s -> {
+            Optional.ofNullable(s.getIncremental()).ifPresent(si -> {
+                request.setIncremental(si);
+                overridePropertiesMap.put("incremental", si.toString());
+            });
 
-            if (override.getSast().getForceScan() != null) {
-                request.setForceScan(override.getSast().getForceScan());
-                overridePropertiesMap.put("force scan", override.getSast().getForceScan().toString());
-            }
+            Optional.ofNullable(s.getForceScan()).ifPresent(sf -> {
+                request.setForceScan(sf);
+                overridePropertiesMap.put("force scan", sf.toString());
+            }); 
 
-            if (!ScanUtils.empty(override.getSast().getPreset())) {
-                request.setScanPreset(override.getSast().getPreset());
+            Optional.ofNullable(s.getPreset()).ifPresent(sp -> {
+                request.setScanPreset(sp);
                 request.setScanPresetOverride(true);
-                overridePropertiesMap.put("scan preset", override.getSast().getPreset());
-            }
-            if (override.getSast().getFolderExcludes() != null) {
-                request.setExcludeFolders(Arrays.asList(override.getSast().getFolderExcludes().split(",")));
-                overridePropertiesMap.put("exclude folders", override.getSast().getFolderExcludes());
-            }
-            if (override.getSast().getFileExcludes() != null) {
-                request.setExcludeFiles(Arrays.asList(override.getSast().getFileExcludes().split(",")));
-                overridePropertiesMap.put("exclude files", override.getSast().getFileExcludes());
-            }
-        }
+                overridePropertiesMap.put("scan preset", sp);
+            });
+            Optional.ofNullable(s.getFolderExcludes()).ifPresent(sfe -> {
+                request.setExcludeFolders(Arrays.asList(sfe.split(",")));
+                overridePropertiesMap.put("exclude folders", sfe);
+            });
+            Optional.ofNullable(s.getFileExcludes()).ifPresent(sf -> {
+                request.setExcludeFiles(Arrays.asList(sf.split(",")));
+                overridePropertiesMap.put("exclude files", sf);
+            });
+        });
+       
         try {
-            if (override.getAdditionalProperties() != null) {
-                Object flow = override.getAdditionalProperties().get("cxFlow");
+            Optional.ofNullable(override.getAdditionalProperties()).ifPresent(ap -> {
+                Object flow = ap.get("cxFlow");
                 ObjectMapper mapper = new ObjectMapper();
                 FlowOverride flowOverride = mapper.convertValue(flow, FlowOverride.class);
 
-                if (flowOverride != null) {
-                    BugTracker bt = request.getBugTracker();
-                    //initial bt as NONE
-                    if(bt == null) {
-                        bt = BugTracker.builder()
+                Optional.ofNullable(flowOverride).ifPresent(fo -> {
+                    BugTracker bt = Optional.ofNullable(request.getBugTracker())
+                        .orElse(BugTracker.builder()
                                 .type(BugTracker.Type.NONE)
-                                .build();
-                    }
-                    String bug = flowOverride.getBugTracker();
+                                .build());
+
+                    String bug = fo.getBugTracker();
                     if(bug != null && !bug.equalsIgnoreCase(bt.getType().toString())) {
                         BugTracker.Type bugType = ScanUtils.getBugTypeEnum(bug, flowProperties.getBugTrackerImpl());
+                        
+                        BugTrackerBuilder builder = BugTracker.builder()
+                            .type(bugType);
                         if (bugType.equals(BugTracker.Type.CUSTOM)) {
-                            bt = BugTracker.builder()
-                                    .type(bugType)
-                                    .customBean(bug)
-                                    .build();
-                        } else {
-                            bt = BugTracker.builder()
-                                    .type(bugType)
-                                    .build();
-                        }
-                        overridePropertiesMap.put("bug tracker", flowOverride.getBugTracker());
+                            builder.customBean(bug);
+                        } 
+                        bt = builder.build();
+                        overridePropertiesMap.put("bug tracker", fo.getBugTracker());
                     }
                     /*Override only applicable to Simple JIRA bug*/
-                    if (bt.getType().equals(BugTracker.Type.JIRA) && flowOverride.getJira() != null) {
-                        overrideJiraBugProperties(flowOverride, bt);
+                    if (bt.getType().equals(BugTracker.Type.JIRA) && fo.getJira() != null) {
+                        overrideJiraBugProperties(fo, bt);
                     }
 
                     request.setBugTracker(bt);
 
-                    if (!ScanUtils.empty(flowOverride.getApplication())) {
-                        request.setApplication(flowOverride.getApplication());
-                        overridePropertiesMap.put("application", flowOverride.getApplication());
-                    }
+                    Optional.ofNullable(fo.getApplication())
+                    .filter(ne -> !empty(ne))
+                    .ifPresent(a -> {
+                        request.setApplication(a);
+                        overridePropertiesMap.put("application", a);
+                    });
 
-                    if (!ScanUtils.empty(flowOverride.getBranches())) {
-                        request.setActiveBranches(flowOverride.getBranches());
-                        overridePropertiesMap.put("active branches", flowOverride.getBranches().toArray().toString());
-                    }
+                    Optional.ofNullable(fo.getBranches())
+                    .filter(ne -> !empty(ne))
+                    .ifPresent(br -> {
+                        request.setActiveBranches(br);
+                        overridePropertiesMap.put("active branches", br.toArray().toString());
+                    });
 
-                    List<String> emails = flowOverride.getEmails();
-                    if (emails != null) {
-                        if (emails.isEmpty()) {
-                            request.setEmail(null);
+                    Optional.ofNullable(fo.getEmails())
+                    .ifPresent(e -> request.setEmail(e.isEmpty() ? null : e));
+
+                    Optional.ofNullable(fo.getFilters()).ifPresent(f -> {
+                        if (!(ScanUtils.empty(f.getSeverity()) && ScanUtils.empty(f.getCwe()) &&
+                                        ScanUtils.empty(f.getCategory()) && ScanUtils.empty(f.getStatus()))) {
+                            List<Filter> filters = ScanUtils.getFilters(f.getSeverity(), f.getCwe(),
+                                    f.getCategory(), f.getStatus());
+                            request.setFilters(filters);
+                            overridePropertiesMap.put("filters", filters.stream().map(Object::toString).collect(Collectors.joining(",")));
                         } else {
-                            request.setEmail(emails);
+                            request.setFilters(null);
+                            overridePropertiesMap.put("filters", "EMPTY");
                         }
-                    }
-                    FlowOverride.Filters filtersObj = flowOverride.getFilters();
+                    });
 
-                    if (filtersObj != null &&
-                            (!ScanUtils.empty(filtersObj.getSeverity()) || !ScanUtils.empty(filtersObj.getCwe()) ||
-                                    !ScanUtils.empty(filtersObj.getCategory()) || !ScanUtils.empty(filtersObj.getStatus()))) {
-                        List<Filter> filters = ScanUtils.getFilters(filtersObj.getSeverity(), filtersObj.getCwe(),
-                                filtersObj.getCategory(), filtersObj.getStatus());
-                        request.setFilters(filters);
-                        overridePropertiesMap.put("filters", filters.stream().map(Object::toString).collect(Collectors.joining(",")));
-                    } else if (filtersObj != null) {
-                        request.setFilters(null);
-                        overridePropertiesMap.put("filters", "EMPTY");
-                    }
-
-                    FlowOverride.Thresholds thresholds = flowOverride.getThresholds();
-
-                    if (thresholds != null &&
-                            !(thresholds.getHigh()==null && thresholds.getMedium()==null &&
-                                    thresholds.getLow()==null && thresholds.getInfo()==null)) {
-
-                            Map<FindingSeverity,Integer> thresholdsMap = ScanUtils.getThresholdsMap(thresholds);
-                            if(!thresholdsMap.isEmpty()) {
-                                flowProperties.setThresholds(thresholdsMap);
-                            }
-
-                        overridePropertiesMap.put("thresholds", convertMapToString(thresholdsMap));
-                    }
-                }
-            }
+                    Optional.ofNullable(flowOverride.getThresholds()).ifPresent(th -> {                    
+                        if ( !(
+                            th.getHigh()==null && 
+                            th.getMedium()==null &&
+                            th.getLow()==null && 
+                            th.getInfo()==null
+                            )) {
+                                Map<FindingSeverity,Integer> thresholdsMap = ScanUtils.getThresholdsMap(th);
+                                if(!thresholdsMap.isEmpty()) {
+                                    flowProperties.setThresholds(thresholdsMap);
+                                }
+    
+                            overridePropertiesMap.put("thresholds", convertMapToString(thresholdsMap));
+                        }
+                    });
+                });
+            });
 
             String overridePropertiesString = convertMapToString(overridePropertiesMap);
 
