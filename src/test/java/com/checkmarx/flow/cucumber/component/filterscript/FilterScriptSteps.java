c52be94376fc707f891a3c5bfcb315993b60b009
AlexeyK <alexey.kononov@checkmarx.com>
2020-06-11 15:50:15 +0300

    Implemented main test scenario for scripted filter. Removed unused files.

--- src/test/java/com/checkmarx/flow/cucumber/component/filterscript/FilterScriptSteps.java
+++ src/test/java/com/checkmarx/flow/cucumber/component/filterscript/FilterScriptSteps.java
@@ -5,25 +5,33 @@ import com.checkmarx.flow.config.FlowProperties;
 import com.checkmarx.flow.cucumber.common.Constants;
 import com.checkmarx.flow.cucumber.common.utils.TestUtils;
 import com.checkmarx.flow.service.FilterFactory;
+import com.checkmarx.sdk.config.CxProperties;
 import com.checkmarx.sdk.dto.ScanResults;
+import com.checkmarx.sdk.dto.cx.CxScanSummary;
 import com.checkmarx.sdk.dto.filtering.FilterConfiguration;
 import com.checkmarx.sdk.exception.CheckmarxException;
-import com.checkmarx.sdk.service.CxClient;
+import com.checkmarx.sdk.service.*;
 import com.checkmarx.utils.TestsParseUtils;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.spring.CucumberContextConfiguration;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
@@ -32,64 +40,138 @@ import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest(classes = {CxFlowApplication.class})
 @RequiredArgsConstructor
+@CucumberContextConfiguration
+@Slf4j
 public class FilterScriptSteps {
-    private String sastReportFilename;
-    private final FlowProperties flowProperties;
-    private final CxClient cxClient;
-    private final FilterFactory filterFactory;
-    private Set<String> filenames;
-    private Map<Integer, String> findingNumberToFilename = new HashMap<>();
+    private final static String SAST_REPORT_FILENAME = "3-findings-filter-script-test.xml";
 
-    @Given("SAST report containing {int} findings, each in a different file and with a different vulnerability type")
-    public void inputContainingFindings(int ignored) {
-        sastReportFilename = "3-findings-filter-script-test.xml";
+    private final FlowProperties flowProperties;
+    private final FilterFactory filterFactory;
+    private Map<String, Integer> findingFilenameToNumber;
+    private final CxProperties cxProperties;
+    private final CxLegacyService cxLegacyService;
+    private final FilterValidator filterValidator;
+    private Set<Integer> findingNumbersAfterFiltering;
+    private String sastReportPath;
+
+    @Given("SAST report containing 3 findings, each in a different file and with a different vulnerability type")
+    public void inputContainingFindings() {
+        sastReportPath = Paths.get(Constants.SAMPLE_SAST_RESULTS_DIR, SAST_REPORT_FILENAME).toString();
     }
 
     @And("finding #{int} has {string} severity, {string} status and {string} state")
-    public void findingHasSeverityStatusAndState(int findingNumber, String severity, String status, String state) throws IOException, ParserConfigurationException, SAXException {
-        Path relativePath = Paths.get(Constants.SAMPLE_SAST_RESULTS_DIR, sastReportFilename);
-        String reportText = TestUtils.getResourceAsString(relativePath.toString());
+    public void findingHasSeverityStatusAndState(int findingNumber, String severity, String status, String state)
+            throws IOException, ParserConfigurationException, SAXException {
 
-        Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(reportText);
-        NodeList findings = doc.getElementsByTagName("Result");
-        for (int i = 0; i < findings.getLength(); i++) {
-            String filename = findings.item(i).getAttributes().getNamedItem("FileName").getTextContent();
-            findingNumberToFilename.put(i + 1, filename);
-        }
-
+        NodeList findings = getXmlFindingsFromResource(sastReportPath);
+        findingFilenameToNumber = mapFilenamesToNumbers(findings);
     }
 
-    @When("CxFlow generates issues from the findings using {string}")
-    public void parsingTheInputWith(String scriptText) throws CheckmarxException {
+    private static NodeList getXmlFindingsFromResource(String path) throws IOException, SAXException, ParserConfigurationException {
+        File sastReportFile = TestUtils.getFileFromResource(path);
+        Document parsedReport = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(sastReportFile);
+        return parsedReport.getElementsByTagName("Result");
+    }
+
+    @And("no simple filters are defined")
+    public void noSimpleFilters() {
+        flowProperties.setFilterSeverity(null);
+        flowProperties.setFilterCwe(null);
+        flowProperties.setFilterCategory(null);
+        flowProperties.setFilterStatus(null);
+        flowProperties.setFilterState(null);
+    }
+
+    @And("filter script is {string}")
+    public void filterScriptId(String scriptText) {
+        if (scriptText.equals("<not specified>")) {
+            scriptText = null;
+        }
+        log.info("Using filter script: '{}'", scriptText);
         flowProperties.setFilterScript(scriptText);
-        FilterConfiguration filter = filterFactory.getFilter(null, null, null, null, null, flowProperties);
-        ScanResults report = cxClient.getReportContent(333, filter);
-        filenames = report.getXIssues()
+    }
+
+    @When("CxFlow generates issues from the findings")
+    public void cxFlowGeneratesIssues() throws CheckmarxException, IOException {
+        FilterConfiguration filter = filterFactory.getFilter(null,
+                null,
+                null,
+                null,
+                null,
+                flowProperties);
+
+        // Avoid additional API calls that we don't care about.
+        cxProperties.setOffline(true);
+
+        RestTemplate restTemplateMock = getRestTemplateMock();
+        CxAuthClient authClientMock = getAuthClientMock();
+
+        CxClient cxClientSpy = getCxClientSpy(restTemplateMock, authClientMock);
+
+        ScanResults report = cxClientSpy.getReportContent(333333, filter);
+
+        findingNumbersAfterFiltering = report.getXIssues()
                 .stream()
-                .map(ScanResults.XIssue::getFilename)
+                .map(xIssue -> findingFilenameToNumber.get(xIssue.getFilename()))
                 .collect(Collectors.toSet());
     }
 
     @Then("CxFlow report is generated with issues corresponding to these findings: {string}")
     public void cxflowReportIsGenerated(String issueDescription) {
         if (issueDescription.equals("<none>")) {
-            assertTrue(filenames.isEmpty());
+            assertTrue(findingNumbersAfterFiltering.isEmpty());
         } else {
-            Set<String> expectedFilenames = TestsParseUtils
-                    .parseCsvToList(issueDescription)
-                    .stream()
-                    .map(findingNumber -> {
-                        int key = Integer.parseInt(findingNumber);
-                        return findingNumberToFilename.get(key);
-                    })
-                    .collect(Collectors.toSet());
-
-            Set<String> actualFilenames = filenames;
-
-            assertEquals(actualFilenames, expectedFilenames);
+            Set<Integer> expectedFindingNumbers = TestsParseUtils.parseCsvToList(issueDescription).stream().map(Integer::parseInt).collect(Collectors.toSet());
+            assertEquals(expectedFindingNumbers, findingNumbersAfterFiltering);
         }
+    }
+
+    /**
+     * Remember which filename corresponds to which finding number, in no particular order.
+     * This will later allow to map resulting XIssues back to finding numbers, since the filename is preserved during
+     * finding -> xIssue conversion.
+     */
+    private static Map<String, Integer> mapFilenamesToNumbers(NodeList findings) {
+        Map<String, Integer> result = new HashMap<>();
+        for (int i = 0; i < findings.getLength(); i++) {
+            String filename = findings.item(i)
+                    .getAttributes()
+                    .getNamedItem("FileName")
+                    .getTextContent();
+            result.put(filename, i + 1);
+        }
+        return result;
+    }
+
+    private CxClient getCxClientSpy(RestTemplate restTemplateMock, CxAuthClient authClientMock) throws CheckmarxException {
+        CxClient cxClient = new CxService(authClientMock,
+                cxProperties,
+                cxLegacyService,
+                restTemplateMock,
+                null,
+                filterValidator);
+
+        CxClient cxClientSpy = spy(cxClient);
+        doReturn(new CxScanSummary()).when(cxClientSpy).getScanSummaryByScanId(any());
+        return cxClientSpy;
+    }
+
+    private CxAuthClient getAuthClientMock() {
+        CxAuthClient authClientMock = mock(CxAuthClient.class);
+        doReturn(new HttpHeaders()).when(authClientMock).createAuthHeaders();
+        doReturn(null).when(authClientMock).getLegacySession();
+        return authClientMock;
+    }
+
+    private RestTemplate getRestTemplateMock() throws IOException {
+        RestTemplate restTemplateMock = mock(RestTemplate.class);
+        String sastReportText = TestUtils.getResourceAsString(sastReportPath);
+        ResponseEntity<String> sastReportEntity = new ResponseEntity<>(sastReportText, HttpStatus.OK);
+        doReturn(sastReportEntity).when(restTemplateMock).exchange(any(), any(), any(), (Class<Object>) any(), any(Integer.class));
+        return restTemplateMock;
     }
 }
--- src/test/resources/cucumber/features/componentTests/filter-script.feature
+++ src/test/resources/cucumber/features/componentTests/filter-script.feature
@@ -5,28 +5,30 @@ Feature: Using Groovy script to filter SAST findings
     And finding #1 has "High" severity, "ReOccured" status and "To Verify" state
     And finding #2 has "Medium" severity, "New" status and "Urgent" state
     And finding #3 has "Low" severity, "New" status and "To verify" state
-    When CxFlow generates issues from the findings using "<filter script>"
+    And no simple filters are defined
+    And filter script is "<filter script>"
+    When CxFlow generates issues from the findings
     Then CxFlow report is generated with issues corresponding to these findings: "<findings>"
-    # finding.status values are currently a union of "NEW" and the values from FilterValidatorImpl.STATE_MAP
     Examples:
-      | filter script                                                                                        | findings |
-      | <not specified>                                                                                      | 1,2,3    |
-      | true                                                                                                 | 1,2,3    |
-      | false                                                                                                | <none>   |
-      | finding.severity == 'HIGH' \|\| (finding.severity == 'MEDIUM' && finding.status == 'URGENT')         | 1,2      |
-      | finding.severity == 'HIGH' \|\| (finding.severity == 'MEDIUM' && finding.status == 'NOTEXPLOITABLE') | 1,2      |
-      | finding.severity != 'TO VERIFY'                                                                      | 2        |
-      | finding.status == 'NEW'                                                                              | 2,3      |
-      | finding.status == 'REOCCURED'                                                                        | <none>   |
+      | filter script                                                                               | findings |
+      | <not specified>                                                                             | 1,2,3    |
+      | true                                                                                        | 1,2,3    |
+      | false                                                                                       | <none>   |
+      | finding.severity == 'HIGH' \|\| (finding.severity == 'MEDIUM' && finding.state == 'URGENT') | 1,2      |
+      | finding.severity == 'HIGH' \|\| (finding.severity == 'MEDIUM' && finding.status == 'NEW')   | 1,2      |
+      | finding.state != 'TO VERIFY'                                                                | 2        |
+      | finding.status == 'NEW'                                                                     | 2,3      |
+      | finding.status == 'REOCCURED'                                                               | 1        |
 
+  @Skip
   @NegativeTest
   Scenario: Both scripted filter and simple filters are specified in config
     # Check that an error is thrown
-
+  @Skip
   @NegativeTest
   Scenario: Invalid script syntax
     # Throw an exception with a meaningful message
-
+  @Skip
   @NegativeTest
   Scenario: Script runtime error
     # E.g. script uses a non-existent variable
