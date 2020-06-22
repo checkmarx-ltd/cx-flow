5afa300acf5a64595cf4fd7d8f79cb3c2f665fa5
AlexeyK <alexey.kononov@checkmarx.com>
2020-06-22 15:47:32 +0300

    Extended ControllerRequest.

--- src/main/java/com/checkmarx/flow/dto/ControllerRequest.java
+++ src/main/java/com/checkmarx/flow/dto/ControllerRequest.java
@@ -12,4 +12,18 @@ import java.util.List;
 public class ControllerRequest {
     private String application;
     private List<String> branch;
+    private List<String> severity;
+    private List<String> cwe;
+    private List<String> category;
+    private String project;
+    private String team;
+    private List<String> status;
+    private String assignee;
+    private String preset;
+    private Boolean incremental;
+    private List<String> excludeFiles;
+    private List<String> excludeFolders;
+    private String override;    // never used
+    private String bug;
+    private Boolean appOnly;
 }
