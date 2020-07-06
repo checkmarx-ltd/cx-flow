53ba611f3f359bbc6220ff5e86806950a015e4cb
NimrodGolan <nimrod.golan@checkmarx.com>
2020-07-06 16:09:44 +0300

    Support SCA properties for Cx-Config

---
+++ src/main/java/com/checkmarx/flow/dto/ScaOverride.java
@@ -0,0 +1,13 @@
+package com.checkmarx.flow.dto;
+
+import com.checkmarx.sdk.dto.Sca;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ScaOverride extends Sca {
+
+}
