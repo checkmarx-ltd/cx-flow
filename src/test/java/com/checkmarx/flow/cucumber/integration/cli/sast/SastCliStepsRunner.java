f7cb90035d992cf0ee109c9d2d033b5de7502c77
NimrodGolan <nimrod.golan@checkmarx.com>
2020-07-27 08:26:21 +0300

    SAST CLI tests impl

--- src/test/java/com/checkmarx/flow/cucumber/integration/cli/sast/SastCliStepsRunner.java
+++ src/test/java/com/checkmarx/flow/cucumber/integration/cli/sast/SastCliStepsRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/cucumber/features/integrationTests/cli/sastCliScan.feature",
-        glue = {"com.checkmarx.flow.cucumber.integration.cli"},
+        glue = {"com.checkmarx.flow.cucumber.integration.cli.sast"},
         tags = "@SAST_CLI_SCAN and not @Skip")
 public class SastCliStepsRunner {
 }
