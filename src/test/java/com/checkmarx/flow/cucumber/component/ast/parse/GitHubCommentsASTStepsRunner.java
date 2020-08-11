59ac87b7cfe9b26443ad628d49971e4eff81d4af
DM\orlyk <orly.kilidgan@checkmarx.com>
2020-08-11 11:29:38 +0300

    change location of tests

--- src/test/java/com/checkmarx/flow/cucumber/component/ast/parse/GitHubCommentsASTStepsRunner.java
+++ src/test/java/com/checkmarx/flow/cucumber/component/ast/parse/GitHubCommentsASTStepsRunner.java
@@ -1,4 +1,4 @@
-package com.checkmarx.flow.cucumber.component.parse.ast;
+package com.checkmarx.flow.cucumber.component.ast.parse;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @CucumberOptions(plugin = { "pretty", "summary", "html:build/cucumber/component/api", "json:build/cucumber/component/api/cucumber.json" },
         features = "classpath:cucumber/features/componentTests/astComments.feature",
-        glue = { "com.checkmarx.flow.cucumber.component.parse.ast" },
+        glue = { "com.checkmarx.flow.cucumber.component.ast.parse" },
         tags = "@Component and not @Skip")
 
 public class GitHubCommentsASTStepsRunner {
