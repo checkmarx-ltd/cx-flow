c2db23b66f05a2b765dd96bf6e0748dfaac1bc9b
Ken McDonald <ken.mcdonald@checkmarx.com>
2020-08-12 22:31:40 -0400

    Fixing code smell for HashMap initialization

--- src/main/java/com/checkmarx/flow/config/SarifProperties.java
+++ src/main/java/com/checkmarx/flow/config/SarifProperties.java
@@ -4,6 +4,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
+import javax.annotation.PostConstruct;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,14 +19,15 @@ public class SarifProperties {
     private String sarifSchema="https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json";
     private String sarifVersion = "2.1.0";
     private String semanticVersion = "1.0.0";
-    private Map<String, String> severityMap = new HashMap<String, String>(){
-        {
-            put("High", "error");
-            put("Medium", "error");
-            put("Low", "warning");
-            put("Informational", "warning");
-        }
-    };
+    private Map<String, String> severityMap = new HashMap<>();
+
+    @PostConstruct
+    private void loadSeverityMap(){
+        severityMap.put("High", "error");
+        severityMap.put("Medium", "error");
+        severityMap.put("Low", "warning");
+        severityMap.put("Informational", "warning");
+    }
 
     public String getFilePath() {
         return filePath;
