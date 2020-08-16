54811c0475ef4c4198e227a13b8312e0fed6e93f
NimrodGolan <nimrod.golan@checkmarx.com>
2020-08-16 12:07:01 +0300

    Set DTO getters & setters

--- src/main/java/com/checkmarx/flow/config/OptionalScmInstanceProperties.java
+++ src/main/java/com/checkmarx/flow/config/OptionalScmInstanceProperties.java
@@ -1,41 +1,14 @@
 package com.checkmarx.flow.config;
 
-public class OptionalScmInstanceProperties {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+class OptionalScmInstanceProperties {
 
     private String webhookToken;
     private String token;
     private String url;
     private String apiUrl;
-
-    public String getWebhookToken() {
-        return webhookToken;
-    }
-
-    public void setWebhookToken(String webhookToken) {
-        this.webhookToken = webhookToken;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getApiUrl() {
-        return apiUrl;
-    }
-
-    public void setApiUrl(String apiUrl) {
-        this.apiUrl = apiUrl;
-    }
 }
