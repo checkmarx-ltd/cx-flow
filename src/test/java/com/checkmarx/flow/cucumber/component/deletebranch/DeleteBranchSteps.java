76271739c616d379c019f29bf46d25dff2f4a6f2
Jeff Armstrong <jarmstrong@nezasoft.com>
2020-09-09 17:58:56 -0700

    Fixed cucumber test
    Test was not handling shard manager variable correctly.

--- src/test/java/com/checkmarx/flow/cucumber/component/deletebranch/DeleteBranchSteps.java
+++ src/test/java/com/checkmarx/flow/cucumber/component/deletebranch/DeleteBranchSteps.java
@@ -327,7 +327,7 @@ public class DeleteBranchSteps {
         ProjectNameGenerator projectNameGeneratorSpy = spy(new ProjectNameGenerator(helperService, cxProperties, null));
             initProjectNameGeneratorSpy(projectNameGeneratorSpy);
 
-        ScanRequestConverter scanRequestConverter = new ScanRequestConverter(helperService, cxProperties, cxClientMock, flowProperties, gitHubService, null, null, null);
+        ScanRequestConverter scanRequestConverter = new ScanRequestConverter(helperService, cxProperties, cxClientMock, flowProperties, gitHubService, null, null, null, null);
         SastScanner sastScanner = new SastScanner(null, cxClientMock, helperService, cxProperties, flowProperties, null, emailService, scanRequestConverter, bugTrackerEventTrigger, projectNameGeneratorSpy);
         List<VulnerabilityScanner> scanners= new LinkedList<>();
         scanners.add(sastScanner);
