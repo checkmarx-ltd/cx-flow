e8fa153cf0dee77a34369e2c4c8574b1c72004f4
Alexey K <alex-ko-dev@users.noreply.github.com>
2020-09-17 12:01:33 +0300

    Added login API support for AST (#421)

--- src/test/java/com/checkmarx/flow/cucumber/integration/ast/scans/AstRemoteRepoScanSteps.java
+++ src/test/java/com/checkmarx/flow/cucumber/integration/ast/scans/AstRemoteRepoScanSteps.java
@@ -2,27 +2,21 @@ package com.checkmarx.flow.cucumber.integration.ast.scans;
 
 import com.checkmarx.flow.CxFlowApplication;
 import com.checkmarx.flow.config.FlowProperties;
-import com.checkmarx.flow.config.GitHubProperties;
-
-import com.checkmarx.flow.cucumber.integration.ast.AstCommonSteps;
-
 import com.checkmarx.flow.cucumber.integration.sca_scanner.ScaCommonSteps;
 import com.checkmarx.flow.dto.ScanRequest;
-
 import com.checkmarx.flow.service.*;
-
 import com.checkmarx.sdk.config.AstProperties;
 import com.checkmarx.sdk.config.CxProperties;
 import com.checkmarx.sdk.config.ScaProperties;
 import com.checkmarx.sdk.dto.ScanResults;
-
-import com.cx.restclient.AstClientImpl;
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
-
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.mockito.invocation.InvocationOnMock;
@@ -30,7 +24,10 @@ import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import static org.junit.Assert.*;
@@ -41,113 +38,149 @@ import static org.mockito.Mockito.when;
 @CucumberContextConfiguration
 @SpringBootTest(classes = {CxFlowApplication.class})
 @Slf4j
-public class AstRemoteRepoScanSteps extends AstCommonSteps {
-
+@RequiredArgsConstructor
+public class AstRemoteRepoScanSteps {
+    final String EMPTY_STRING_INDICATOR = "<empty>";
     private static final String PUBLIC_PROJECT_NAME = "Public-Test-Test-Repo";
 
     private static final String PUBLIC_REPO = "https://github.com/cxflowtestuser/testsAST.git";
     private static final String SEPARATOR = ",";
-    private static final String EXPIRED_TOKEN = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJrdkdrM1k5ODBrM29VV0VxM2NscjlOeEIwbTlCbE90N3k0TlRWbHBRRFFNIn0.eyJleHAiOjE1OTU5NTI0MTgsImlhdCI6MTU5NTkxNjQxOCwiYXV0aF90aW1lIjoxNTk1OTE2NDE4LCJqdGkiOiI4MDc5NzAyMy0xMzJlLTQzODktYmE0Yy04YjEzOTc4NTcwZTgiLCJpc3MiOiJodHRwOi8vMTAuMzIuNC4zMy9hdXRoL3JlYWxtcy9vcmdhbml6YXRpb24iLCJhdWQiOlsicmVhbG0tbWFuYWdlbWVudCIsImFjY291bnQiXSwic3ViIjoiMTg4MzE1MWUtNzI5Yi00MTQ4LThjYmUtYzcxOTUwMWEzNDRkIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiYXN0LWFwcCIsIm5vbmNlIjoiNDliYmNhYjctOGUzYy00OWIxLTlkYTgtZWVlMjE5NTg5NGJmIiwic2Vzc2lvbl9zdGF0ZSI6IjU5NGJhMTliLTU5NDUtNDgxMC05ZTYxLTc1ZjBlNWEzNThkMSIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiKiJdLCJzY29wZSI6Im9wZW5pZCBpYW0tYXBpIHByb2ZpbGUgZW1haWwgYXN0LWFwaSByb2xlcyIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiIsInVzZXIiLCJpYW0tYWRtaW4iXSwicm9sZXNfYXN0IjpbImNyZWF0ZS1wcm9qZWN0Iiwidmlldy1wcm9qZWN0cyIsImRlbGV0ZS1hcHBsaWNhdGlvbiIsInZpZXctYXBwbGljYXRpb25zIiwibWFuYWdlLXdvcmtzcGFjZSIsImNyZWF0ZS1hcHBsaWNhdGlvbiIsIm1hbmFnZS1yZXN1bHQiLCJtYW5hZ2UtcHJvamVjdCIsInZpZXctcmVzdWx0cyIsImNyZWF0ZS13b3Jrc3BhY2UiLCJkZWxldGUtcHJvamVjdCIsInZpZXctc2NhbnMiLCJkZWxldGUtd29ya3NwYWNlIiwiYXN0LWFkbWluIiwiY3JlYXRlLXNjYW4iLCJkZWxldGUtc2NhbiIsIm1hbmFnZS1hcHBsaWNhdGlvbiIsInZpZXctd29ya3NwYWNlcyIsInZpZXctaGVhbHRoY2hlY2siLCJ2aWV3LWVuZ2luZeKAi3MiXSwicHJlZmVycmVkX3VzZXJuYW1lIjoib3JnX2FkbWluIn0.Zw4GuU5BKeklBtBQAqmhjqBKo3VHPCzxjan6BFCPHCv70E5o34zjF-Df0eG71c87jYq6hC-7N6ce6m-MH28Ab8cYa1ZOKN22PqEO88WftoYOoHOA-PlSV4NrH9ybAObVymdYNb_LtTpvWGhyv3Dx4G4sEQ6v5N-apOiP5fSoVbCmmzfcVqIjkbWlXm1_h6AUfmyn16bImAYix_YuZAnQdw2Bs19cQFH824Y8-v8oGVrqZbNk9D-7sUuJsamcGWALyv3OF3SaZJndlYaGGsZtL6a6_iLuRiT27k-mAzY306k-4BVbBRoCKSakGEG-0qVcZX1XT8dQHzwiv7qBXT6kVA";
 
-    private static ResultsService resultsService;
+    private ResultsService resultsServiceMock;
 
-    private final GitHubProperties gitHubProperties;
     private final CxProperties cxProperties;
-
-
-    private ScanRequest scanRequest;
+    private final FlowProperties flowProperties;
+    private final AstProperties astProperties;
+    private final ScaProperties scaProperties;
 
     private ScanResults scanResults;
     private boolean isScaEnabled;
     private boolean isAstEnabled;
+    private Exception scanException;
 
     @Autowired
     private SCAScanner scaScanner;
-    
+
     @Autowired
     private ASTScanner astScanner;
-    
-    public AstRemoteRepoScanSteps(FlowProperties flowProperties, AstProperties astProperties, ScaProperties scaProperteis,
-                                  GitHubProperties gitHubProperties, CxProperties cxProperties) {
-        super(flowProperties, astProperties, scaProperteis);
-        this.gitHubProperties = gitHubProperties;
-        this.cxProperties = cxProperties;
 
-    }
+    private String validClientId;
+    private String validClientSecret;
 
-    /**
-     * Returns scan results as if they were produced by SAST.
-     */
-    private class ResultsServiceAnswerer implements Answer<CompletableFuture<ScanResults>> {
-        
+    private class ScanResultsInterceptor implements Answer<CompletableFuture<ScanResults>> {
         @Override
         public CompletableFuture<ScanResults> answer(InvocationOnMock invocation) {
-
-            ScanResults scanResultsLocal = invocation.getArgument(1);
-            scanResults = scanResultsLocal;
+            scanResults = invocation.getArgument(1);
             return null;
         }
     }
  
     @Before("@ASTRemoteRepoScan")
     public void init() {
-        initAstConfig();
         ScaCommonSteps.initSCAConfig(scaProperties);
-        resultsService = mock(ResultsService.class);
-        ResultsServiceAnswerer answerer = new ResultsServiceAnswerer();
-        when(resultsService.publishCombinedResults(any(), any())).thenAnswer(answerer);
+        resultsServiceMock = mock(ResultsService.class);
+        ScanResultsInterceptor answerer = new ScanResultsInterceptor();
+        when(resultsServiceMock.publishCombinedResults(any(), any())).thenAnswer(answerer);
     }
 
-    @Given("AST scan with expired token")
-    public void configureToken(){
-        astProperties.setToken(EXPIRED_TOKEN);
-        flowProperties.setEnabledVulnerabilityScanners(Collections.singletonList(AstProperties.CONFIG_PREFIX));
-        this.isAstEnabled = true;
+    @Before("@InvalidCredentials")
+    public void backupValidCredentials() {
+        log.info("Saving valid credentials.");
+        validClientId = astProperties.getClientId();
+        validClientSecret = astProperties.getClientSecret();
     }
 
-    @Given("AST scan is initiated when AST is not available")
-    public void configureAstInvalidMachine(){
-        
-        astProperties.setApiUrl(cxProperties.getBaseUrl());
-        flowProperties.setEnabledVulnerabilityScanners(Collections.singletonList(AstProperties.CONFIG_PREFIX));
-        this.isAstEnabled = true;
+    @After("@InvalidCredentials")
+    public void restoreValidCredentials() {
+        // The @InvalidCredentials changes credentials in astProperties,
+        // so here we need to restore them for other tests to work.
+        log.info("Restoring valid credentials.");
+        astProperties.setClientId(validClientId);
+        astProperties.setClientSecret(validClientSecret);
     }
-    
-    @Then("expired token expected error will be returned {string}")
-    public void astScanWithExpiredtoken(String error){
+
+    @When("CxFlow tries to start AST scan with the {string} and {string} credentials")
+    public void configureToken(String clientIdDescr, String clientSecretDescr){
+        astProperties.setClientId(toActualClientId(clientIdDescr));
+        astProperties.setClientSecret(toActualClientSecret(clientSecretDescr));
+        isAstEnabled = true;
+        try {
+            startScan(Collections.singletonList(astScanner));
+        } catch (Exception e) {
+            scanException = e;
+        }
+    }
+
+    private String toActualClientSecret(String clientSecretDescr) {
+        String result;
+        if (clientSecretDescr.equals(EMPTY_STRING_INDICATOR)) {
+            result = "";
+        } else if (clientSecretDescr.equals("<valid-secret>")) {
+            result = validClientSecret;
+        } else {
+            result = clientSecretDescr;
+        }
+        return result;
+    }
+
+    private String toActualClientId(String clientIdDescr) {
+        String result;
+        if (clientIdDescr.equals(EMPTY_STRING_INDICATOR)) {
+            result = "";
+        } else if (clientIdDescr.equals("<valid-client-id>")) {
+            result = validClientId;
+        } else {
+            result = clientIdDescr;
+        }
+        return result;
+    }
+
+    @When("AST scan is initiated with API url: {string}")
+    public void configureAstInvalidMachine(String url) {
+        String effectiveUrl = url.replace("<ast url>", astProperties.getApiUrl())
+                .replace("<sast url>", cxProperties.getBaseUrl());
+
+        log.info("Setting url to {}", effectiveUrl);
+        astProperties.setApiUrl(effectiveUrl);
+        flowProperties.setEnabledVulnerabilityScanners(Collections.singletonList(AstProperties.CONFIG_PREFIX));
+        isAstEnabled = true;
+    }
+
+    @Then("an error will be thrown with the message containing {string}")
+    public void errorWillBeThrown(String messageSubstring) {
+        assertNotNull("Expected an exception but didn't get any.", scanException);
+
+        String testFailureMessage = String.format("The exception message ('%s') doesn't contain the expected text ('%s')",
+                scanException.getMessage(),
+                messageSubstring);
+
+        assertTrue(testFailureMessage, StringUtils.containsIgnoreCase(scanException.getMessage(), messageSubstring));
+    }
+
+    @Then("an exception of type {string} will be thrown with the message containing {string}")
+    public void astScanWithInvalidAstMachine(String exceptionType, String errorSubstring) {
         List<VulnerabilityScanner> scanners = new LinkedList<>();
-        scanners.add(new ASTScanner(new AstClientImpl(astProperties),flowProperties));
         scanners.add(astScanner);
         try {
             startScan(scanners);
             fail("no exception was thrown");
-        }catch(Exception e){
-            assertTrue(e.getMessage().contains(error));
+        } catch (Exception e) {
+            assertEquals("Unexpected exception type.",exceptionType, e.getClass().getSimpleName());
+
+            String message = String.format("Expected error message to contain '%s'.", errorSubstring);
+            boolean containsSubstring = StringUtils.containsIgnoreCase(e.getMessage(), errorSubstring);
+            assertTrue(message, containsSubstring);
         }
     }
 
-    @Then("unavailable AST server expected error will be returned {string}")
-    public void astScanWithInvalidAstMachine(String error){
-        List<VulnerabilityScanner> scanners = new LinkedList<>();
-        scanners.add(new ASTScanner(new AstClientImpl(astProperties),flowProperties));
-        scanners.add(astScanner);
-        try {
-            startScan(scanners);
-            fail("no exception was thrown");
-        }catch(Exception e){
-            assertTrue(e.getMessage().contains(error));
-        }
-    }
-
-    @Given("scan initiator list is {string}")
+    @Given("enabled vulnerability scanners are {string}")
     public void setScanInitiator(String initiatorList) {
         String[] intiators ;
         List<VulnerabilityScanner> scanners = new LinkedList<>();
         if(!initiatorList.contains(SEPARATOR)){
             intiators = new String[1];
             intiators[0] = initiatorList;
-        }
-        else{
+        } else {
             intiators = initiatorList.split(SEPARATOR);
         }
         for (String scanType: intiators) {
@@ -160,7 +193,6 @@ public class AstRemoteRepoScanSteps extends AstCommonSteps {
             if(scanType.equalsIgnoreCase(AstProperties.CONFIG_PREFIX)){
                 flowProperties.setEnabledVulnerabilityScanners(Collections.singletonList(AstProperties.CONFIG_PREFIX));
                 this.isAstEnabled = true;
-                //scanners.add(new ASTScanner(new AstClientImpl(astProperties),flowProperties));
                 scanners.add(astScanner);
             }
             
@@ -171,49 +203,58 @@ public class AstRemoteRepoScanSteps extends AstCommonSteps {
     }
 
 
-    @Then("the returned contain populated results for all initiators")
+    @Then("scan results contain populated results for all scanners")
     public void validateResults() {
         if(isScaEnabled) {
-            assertNotNull("SCA results are not null.", scanResults.getScaResults());
-            assertNotEquals("SCA scan ID is not empty", StringUtils.isEmpty(scanResults.getScaResults().getScanId()));
-            assertEquals("Sca vulnerable packages: ", 13, scanResults.getScaResults().getPackages().size() );
+            assertNotNull("SCA results are null", scanResults.getScaResults());
+            assertTrue("SCA scan ID is empty", StringUtils.isNotEmpty(scanResults.getScaResults().getScanId()));
         }
         if(isAstEnabled) {
-            assertNotNull("AST results are not null.", scanResults.getAstResults());
-            assertNotEquals("AST scan ID is not empty", StringUtils.isEmpty(scanResults.getAstResults().getResults().getScanId()));
+            assertNotNull("AST results are null.", scanResults.getAstResults());
+            assertTrue("AST scan ID is empty", StringUtils.isNotEmpty(scanResults.getAstResults().getResults().getScanId()));
         }
      }
-     
-     @And("sca finding count will be {string} and ast findings count {string} will be accordingly")
-     public void validateNumberOfFindings(String scaFindings, String astFindings){
-         if(isScaEnabled) {
-             assertEquals("Sca vulnerable packages: ", 13, scanResults.getScaResults().getPackages().size() );
-             assertEquals("Sca findings: ", 13, scanResults.getScaResults().getFindings().size() );
 
-         }
-         if(isAstEnabled) {
-             assertEquals("AST findings: ", 11, scanResults.getAstResults().getResults().getFindings().size() );
+    @And("sca finding count will be {string} and ast findings count {string} will be accordingly")
+    public void validateNumberOfFindings(String scaFindings, String astFindings) {
+        if (isScaEnabled) {
+            validateFindingCount(scaFindings, scanResults.getScaResults().getFindings(), "SCA");
+        }
+        if (isAstEnabled) {
+            validateFindingCount(astFindings, scanResults.getAstResults().getResults().getFindings(), "AST");
+        }
+    }
 
-         }
-     }
-
+    private void validateFindingCount(String expectedCountDescr, List<?> actualFindings, String scannerType) {
+        int actualSize = actualFindings.size();
+        String message = String.format("Expected that %s finding count would be %s, but got %d.",
+                scannerType,
+                expectedCountDescr,
+                actualSize);
+        boolean countIsExpected = expectedCountDescr.equals("0") ? actualSize == 0 : actualSize > 0;
+        assertTrue(message, countIsExpected);
+    }
 
     public void startScan(List<VulnerabilityScanner> scanners) {
-        
         CxProperties cxProperties = new CxProperties();
         ExternalScriptService scriptService = new ExternalScriptService();
         HelperService helperService = new HelperService(flowProperties, cxProperties, scriptService);
      
         ProjectNameGenerator projectNameGenerator = new ProjectNameGenerator(helperService, cxProperties, scriptService);
-        FlowService flowService = new FlowService(new ArrayList<>(), projectNameGenerator, resultsService);
+        FlowService flowService = new FlowService(new ArrayList<>(), projectNameGenerator, resultsServiceMock);
 
-        scanRequest = getBasicScanRequest(PUBLIC_PROJECT_NAME, PUBLIC_REPO);
+        ScanRequest scanRequest = getBasicScanRequest();
 
         scanRequest.setVulnerabilityScanners(scanners);
         flowService.initiateAutomation(scanRequest);
     }
 
-  
-
-  
+    private static ScanRequest getBasicScanRequest() {
+        return ScanRequest.builder()
+                .project(PUBLIC_PROJECT_NAME)
+                .repoUrlWithAuth(PUBLIC_REPO)
+                .branch("master")
+                .repoType(ScanRequest.Repository.GITHUB)
+                .build();
+    }
 }
--- src/test/resources/cucumber/features/integrationTests/ast/astScanProcessing.feature
+++ src/test/resources/cucumber/features/integrationTests/ast/astScanProcessing.feature
@@ -1,35 +1,44 @@
 @AstIntegrationTests @IntegrationTest
 Feature: Cx-Flow AST Integration permutation tests
-  
 
-  @ASTRemoteRepoScan 
-  Scenario Outline: scan multiple initiators
-    Given scan initiator list is "<initiators>"
-    Then the returned contain populated results for all initiators
+
+  @ASTRemoteRepoScan
+  Scenario Outline: using multiple vulnerability scanners
+    Given enabled vulnerability scanners are "<scanners>"
+    Then scan results contain populated results for all scanners
     And sca finding count will be "<sca_findings>" and ast findings count "<ast_findings>" will be accordingly
 
     Examples:
-      | initiators | sca_findings | ast_findings |
-      | SCA        | 13           | 0            |
-      | AST        | 0            | 11           |
-      | AST,SCA    | 13           | 11           |
+    # Cannot rely on an exact number of findings, because it may change after backend version updates.
+      | scanners | sca_findings | ast_findings |
+      | SCA      | > 0          | 0            |
+      | AST      | 0            | > 0          |
+      | AST,SCA  | > 0          | > 0          |
+
+  @ASTRemoteRepoScan @InvalidCredentials
+  Scenario Outline: Trying to scan with invalid credentials
+    When CxFlow tries to start AST scan with the "<client id>" and "<client secret>" credentials
+    Then an error will be thrown with the message containing "<message>"
+    Examples:
+    # <valid-xxx> values are replaced with actual values from the test config.
+      | client id          | client secret   | message                           |
+      | <valid-client-id>  | my-wrong-secret | unauthorized                      |
+      | my-wrong-client-id | <valid-secret>  | unauthorized                      |
+      | <valid-client-id>  | <empty>         | AST client secret wasn't provided |
+      | <empty>            | <valid-secret>  | AST client ID wasn't provided     |
+
 
   @ASTRemoteRepoScan
-  Scenario Outline: check error message on expired AST token
-    Given AST scan with expired token
-    Then expired token expected error will be returned "<message>"
+  Scenario Outline: AST is not accessible
+    When AST scan is initiated with API url: "<url>"
+    Then an exception of type "MachinaRuntimeException" will be thrown with the message containing "ast scan failed"
     Examples:
-      | message      |
-      | Unauthorized |
-      
-
-  @ASTRemoteRepoScan
-  Scenario Outline: check error message when AST is down
-    Given AST scan is initiated when AST is not available
-    Then unavailable AST server expected error will be returned "<message>"
-    Examples:
-      | message |
-      | The resource you are looking for might have been removed, had its name changed, or is temporarily unavailable    |
+      | url                        |
+      | not a url!                 |
+      | <sast url>                 |
+      | https://example.com        |
+      | <ast url>/nonexistent/path |
+      | http://192.168.199.200     |
 
   @AST_JIRA_issue_creation
   Scenario: Publish AST results and check JIRA tickets are getting created
