90803173ba12044dd6bcd5dac66134b6f21f1b77
orlyk <orly.kilidgan@checkmarx.com>
2020-09-22 15:37:55 +0300

    try fix failing tests

--- src/test/java/com/checkmarx/flow/cucumber/integration/cli/ast/AstCliSteps.java
+++ src/test/java/com/checkmarx/flow/cucumber/integration/cli/ast/AstCliSteps.java
@@ -130,11 +130,12 @@ public class AstCliSteps  {
   
         try {
             TestUtils.runCxFlow(cxFlowRunner, commandBuilder.toString());
+            cxFlowExecutionException = null;
         } catch (Throwable e) {
             cxFlowExecutionException = e;
         }
 
-        cxFlowExecutionException = null;
+        
     }
 
     @Then("run should exit with exit code {}")
@@ -163,17 +164,13 @@ public class AstCliSteps  {
     }
 
     @Then("bug tracker contains {} issues")
-    public void validateBugTrackerIssues(int countIssues) {
+    public void validateBugTrackerIssues(int expectedIssuesCount) {
 
-        int expectedIssueCount = countIssues;
         int actualIssueCount = jiraUtils.getNumberOfIssuesInProject(jiraProperties.getProject());
 
-        log.info("comparing expected number of issues: {}, to actual bug tracker issues; {}", expectedIssueCount, actualIssueCount);
-        if (expectedIssueCount == AT_LEAST_ONE) {
-            Assert.assertTrue("Expected at least one issue in bug tracker.", actualIssueCount > 0);
-        } else {
-            Assert.assertEquals("Wrong issue count in bug tracker.", expectedIssueCount, actualIssueCount);
-        }
+        log.info("comparing expected number of issues: {}, to actual bug tracker issues; {}", expectedIssuesCount, actualIssueCount);
+        Assert.assertEquals("Wrong issue count in bug tracker.", expectedIssuesCount, actualIssueCount);
+        
     }
 
 
