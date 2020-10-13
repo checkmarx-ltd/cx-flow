36e36d41a5aa78ab0af3b1d56ffe198221b2b69d
Ken McDonald <ken.mcdonald@checkmarx.com>
2020-10-12 23:16:46 -0400

    Adding constants for GitHub App

--- src/main/java/com/checkmarx/flow/constants/FlowConstants.java
+++ src/main/java/com/checkmarx/flow/constants/FlowConstants.java
@@ -3,6 +3,8 @@ package com.checkmarx.flow.constants;
 public class FlowConstants {
 
     public static final String MAIN_MDC_ENTRY = "cx";
+    public static final String GITHUB_APP_INSTALLATION_ID = "github-installation-id";
+    public static final String GITHUB_APP_CLONE_USER = "x-access-token";
 
     private FlowConstants() {
     }
