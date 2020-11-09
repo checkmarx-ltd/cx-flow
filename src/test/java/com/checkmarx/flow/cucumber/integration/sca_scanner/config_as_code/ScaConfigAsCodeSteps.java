4853fdc1c917eb8b52add627025fb7dcc9c1421e
AlexeyK <alexey.kononov@checkmarx.com>
2020-11-09 15:09:59 +0200

    Updated SCA config-as-code steps according to the new filter structure.

--- src/test/java/com/checkmarx/flow/cucumber/integration/sca_scanner/config_as_code/ScaConfigAsCodeSteps.java
+++ src/test/java/com/checkmarx/flow/cucumber/integration/sca_scanner/config_as_code/ScaConfigAsCodeSteps.java
@@ -1,28 +1,32 @@
 package com.checkmarx.flow.cucumber.integration.sca_scanner.config_as_code;
 
 import com.checkmarx.flow.CxFlowApplication;
-import com.checkmarx.flow.config.FlowProperties;
 import com.checkmarx.flow.config.GitHubProperties;
 import com.checkmarx.flow.dto.ScanRequest;
 import com.checkmarx.flow.service.ConfigurationOverrider;
 import com.checkmarx.flow.service.GitHubService;
-import com.checkmarx.sdk.config.ScaProperties;
 import com.checkmarx.sdk.dto.CxConfig;
+import com.checkmarx.sdk.dto.Filter;
 import io.cucumber.java.Before;
 import io.cucumber.java.PendingException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootTest(classes = {CxFlowApplication.class})
 @RequiredArgsConstructor
+@Slf4j
 public class ScaConfigAsCodeSteps {
 
     private final GitHubProperties gitHubProperties;
@@ -67,26 +71,43 @@ public class ScaConfigAsCodeSteps {
                     break;
                 case "thresholdsScore":
                     failMsg = "SCA thresholds score from configuration as code is not as expected";
-                    expected = new Double(8.5);
+                    expected = 8.5;
                     actual = scanRequest.getScaConfig().getThresholdsScore();
                     Assert.assertEquals(failMsg , expected , actual);
                     break;
                 case "filterSeverity":
                     failMsg = "SCA filter severity from configuration as code is not as expected";
-                    expected = "[high, medium, low]";
-                    actual = scanRequest.getScaConfig().getFilterSeverity().toString();
-                    Assert.assertEquals(failMsg , expected , actual);
+                    Set<String> actualSeverities = getFilterValues(Filter.Type.SEVERITY)
+                            .collect(Collectors.toSet());
+
+                    Assert.assertTrue(failMsg, actualSeverities.containsAll(Arrays.asList("high", "medium", "low")));
                     break;
                 case "filterScore":
                     failMsg = "SCA filter score from configuration as code is not as expected";
-                    expected = new Double(7.5);
-                    actual = scanRequest.getScaConfig().getFilterScore();
-                    Assert.assertEquals(failMsg , expected , actual);
+                    expected = "7.5";
+                    String actualScore = getFilterValues(Filter.Type.SCORE)
+                            .findFirst()
+                            .orElse(null);
+                    Assert.assertEquals(failMsg, expected, actualScore);
                     break;
                 default:
                     throw new PendingException("The support for this parameter is not yet implemented: " + currentParameter);
             }
         });
+    }
+
+    private Stream<String> getFilterValues(Filter.Type type) {
+        try {
+            return scanRequest.getFilter()
+                    .getScaFilters()
+                    .getSimpleFilters()
+                    .stream()
+                    .filter(filter -> filter.getType() == type)
+                    .map(Filter::getValue);
+        } catch (Exception e) {
+            Assert.fail(String.format("Unable to get SCA filters of type %s: %s", type, e));
+            throw e;
+        }
     }
 
     private void setGitHubProperties() {
