608e013db112dc91d3cf1af02bbc5bb3edaba636
orlyk <orly.kilidgan@checkmarx.com>
2021-01-13 16:00:26 +0200

    remove unnessasary import

--- src/test/java/com/checkmarx/flow/cucumber/integration/pullrequest/updatecomments/UpdatePullRequestCommentsSteps.java
+++ src/test/java/com/checkmarx/flow/cucumber/integration/pullrequest/updatecomments/UpdatePullRequestCommentsSteps.java
@@ -41,7 +41,6 @@ import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.HttpClientErrorException;
-import org.testng.annotations.AfterTest;
 
 import java.io.File;
 import java.io.IOException;
