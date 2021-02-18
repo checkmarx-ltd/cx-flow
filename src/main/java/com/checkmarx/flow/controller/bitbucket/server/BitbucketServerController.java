f61fa6ed5db54773961bb3f28ac67c42b97a1db1
nleach999 <nleach999@gmail.com>
2021-02-18 06:52:01 -0600

    WIP - probably does not compile

---
+++ src/main/java/com/checkmarx/flow/controller/bitbucket/server/BitbucketServerController.java
@@ -0,0 +1,251 @@
+package com.checkmarx.flow.controller.bitbucket.server;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.annotation.PostConstruct;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+
+import com.checkmarx.flow.config.BitBucketProperties;
+import com.checkmarx.flow.config.FlowProperties;
+import com.checkmarx.flow.config.JiraProperties;
+import com.checkmarx.flow.dto.ControllerRequest;
+import com.checkmarx.flow.dto.EventResponse;
+import com.checkmarx.flow.dto.bitbucketserver.PullEvent;
+import com.checkmarx.flow.dto.bitbucketserver.PushEvent;
+import com.checkmarx.flow.exception.InvalidTokenException;
+import com.checkmarx.flow.exception.MachinaRuntimeException;
+import com.checkmarx.flow.service.BitBucketService;
+import com.checkmarx.flow.service.ConfigurationOverrider;
+import com.checkmarx.flow.service.CxScannerService;
+import com.checkmarx.flow.service.FilterFactory;
+import com.checkmarx.flow.service.FlowService;
+import com.checkmarx.flow.service.HelperService;
+import com.checkmarx.flow.utils.ScanUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.slf4j.Logger;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping(value = "/")
+@RequiredArgsConstructor
+public class BitbucketServerController implements ConfigContextProvider {
+
+    private static final String SIGNATURE = "X-Hub-Signature";
+    private static final String EVENT = "X-Event-Key";
+    private static final String PING = EVENT + "=diagnostics:ping";
+    private static final String PUSH = EVENT + "=repo:refs_changed";
+    private static final String MERGE = EVENT + "=pr:opened";
+    private static final String MERGED = EVENT + "=pr:merged";
+    private static final String PR_SOURCE_BRANCH_UPDATED = EVENT + "=pr:from_ref_updated";
+    private static final String HMAC_ALGORITHM = "HMACSha256";
+    protected static final Charset CHARSET = StandardCharsets.UTF_8;
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(BitbucketServerController.class);
+    protected static final int INDEX_FROM_CHANGES = 0;
+    protected static final int INDEX_FROM_SELF = 0;
+
+    private final FlowProperties flowProperties;
+    private final BitBucketProperties bitBucketProperties;
+    private final CxScannerService cxScannerService;
+    private final JiraProperties jiraProperties;
+    private final FlowService flowService;
+    private final HelperService helperService;
+    private final BitBucketService bitbucketService;
+    private final FilterFactory filterFactory;
+    private final ConfigurationOverrider configOverrider;
+
+
+    private Mac hmac;
+
+    @PostConstruct
+    public void init() throws NoSuchAlgorithmException, InvalidKeyException {
+        // initialize HMAC with SHA1 algorithm and secret
+        if (!ScanUtils.empty(bitBucketProperties.getWebhookToken())) {
+            SecretKeySpec secret = new SecretKeySpec(bitBucketProperties.getWebhookToken().getBytes(CHARSET), HMAC_ALGORITHM);
+            hmac = Mac.getInstance(HMAC_ALGORITHM);
+            hmac.init(secret);
+        }
+    }
+
+    @PostMapping(value = {"/{product}", "/"}, headers = PING)
+    public String pingEvent(
+            @PathVariable(value = "product", required = false) String product) {
+        log.info("Processing Bitbucket Server PING request");
+        return "ok";
+    }
+
+    /**
+     * Push Request event webhook submitted.
+     */
+    @PostMapping(value = {"/{product}", "/"}, headers = MERGE)
+    public ResponseEntity<EventResponse> mergeRequest(
+            @RequestBody String body,
+            @PathVariable(value = "product", required = false) String product,
+            @RequestHeader(value = SIGNATURE) String signature,
+            ControllerRequest controllerRequest
+    ) {
+        return doMergeEvent(body, product, signature, controllerRequest);
+    }
+
+    /**
+     * Push Request event webhook submitted.
+     */
+    @PostMapping(value = {"/{product}", "/"}, headers = MERGED)
+    public ResponseEntity<EventResponse> mergedRequest(
+            @RequestBody String body,
+            @PathVariable(value = "product", required = false) String product,
+            @RequestHeader(value = SIGNATURE) String signature,
+            ControllerRequest controllerRequest
+    ) {
+        return doMergeEvent(body, product, signature, controllerRequest);
+    }
+
+    /**
+     * PR Source Branch Updated Request event webhook submitted.
+     */
+    @PostMapping(value = {"/{product}", "/"}, headers = PR_SOURCE_BRANCH_UPDATED)
+    public ResponseEntity<EventResponse> prSourceBranchUpdateRequest(
+            @RequestBody String body,
+            @PathVariable(value = "product", required = false) String product,
+            @RequestHeader(value = SIGNATURE) String signature,
+            ControllerRequest controllerRequest
+    ) {
+        return doMergeEvent(body, product, signature, controllerRequest);
+    }
+
+    private ResponseEntity<EventResponse> doMergeEvent(String body, String product, String signature,
+            ControllerRequest controllerRequest) {
+
+        verifyHmacSignature(body, signature);
+        ObjectMapper mapper = new ObjectMapper();
+        PullEvent event;
+
+        try {
+            event = mapper.readValue(body, PullEvent.class);
+        } catch (IOException e) {
+            throw new MachinaRuntimeException(e);
+        }
+
+        BitbucketServerMergeHandler handler = BitbucketServerMergeHandler.builder()
+        .application(event.getPullRequest().getFromRef().getRepository().getName() )
+        .currentBranch(event.getPullRequest().getFromRef().getDisplayId())
+        .targetBranch(event.getPullRequest().getToRef().getDisplayId())
+        .fromRefLatestCommit(event.getPullRequest().getFromRef().getLatestCommit())
+
+        .webhookPayload(body)
+        .configProvider(this)
+        .product(product)
+        .build();
+
+        return handler.execute();//workflow.doMergeEvent(body, product, controllerRequest);
+    }
+
+
+    /**
+     * Receive Push event submitted from Bitbucket
+     */
+    @PostMapping(value = {"/{product}", "/"}, headers = PUSH)
+    public ResponseEntity<EventResponse> pushRequest(
+            @RequestBody String body,
+            @PathVariable(value = "product", required = false) String product,
+            @RequestHeader(value = SIGNATURE) String signature,
+            ControllerRequest controllerRequest
+
+    ) {
+        verifyHmacSignature(body, signature);
+
+        ObjectMapper mapper = new ObjectMapper();
+        PushEvent event;
+
+        try {
+            event = mapper.readValue(body, PushEvent.class);
+            
+        } catch (IOException e) {
+            throw new MachinaRuntimeException(e);
+        }
+
+        BitbucketServerPushHandler handler = BitbucketServerPushHandler.builder()
+        .controllerRequest(controllerRequest)
+        .application(event.getRepository().getName())
+        .branchFromRef(event.getChanges().get(INDEX_FROM_CHANGES).getRefId())
+        .toHash(event.getChanges().get(INDEX_FROM_CHANGES).getToHash())
+        .email(event.getActor().getEmailAddress())
+        .fromProjectKey(event.getRepository().getProject().getKey())
+        .fromSlug(event.getRepository().getSlug())
+        .toProjectKey(event.getRepository().getProject().getKey())
+        .toSlug(event.getRepository().getSlug())
+        .repositoryName(event.getRepository().getName())
+        .refId(event.getChanges().get(INDEX_FROM_CHANGES).getRefId())
+        .browseUrl(event.getRepository().getLinks().getSelf().get(INDEX_FROM_SELF).getHref())
+        .webhookPayload(body)
+        .configProvider(this)
+        .product(product)
+        .build();
+        
+
+        return handler.execute();
+    }
+
+    /**
+     * Validates the received body using the BB hook secret.
+     */
+    private void verifyHmacSignature(String message, String signature) {
+        byte[] sig = hmac.doFinal(message.getBytes(CHARSET));
+        String computedSignature = "sha256=" + DatatypeConverter.printHexBinary(sig);
+        if (!computedSignature.equalsIgnoreCase(signature)) {
+            throw new InvalidTokenException();
+        }
+        log.info("Signature verified");
+    }
+
+    public FlowProperties getFlowProperties() {
+        return flowProperties;
+    }
+
+    public BitBucketProperties getBitBucketProperties() {
+        return bitBucketProperties;
+    }
+
+    public CxScannerService getCxScannerService() {
+        return cxScannerService;
+    }
+
+    public JiraProperties getJiraProperties() {
+        return jiraProperties;
+    }
+
+    public FlowService getFlowService() {
+        return flowService;
+    }
+
+    public HelperService getHelperService() {
+        return helperService;
+    }
+
+    public BitBucketService getBitbucketService() {
+        return bitbucketService;
+    }
+
+    public FilterFactory getFilterFactory() {
+        return filterFactory;
+    }
+
+    public ConfigurationOverrider getConfigOverrider() {
+        return configOverrider;
+    }
+
+}
---
+++ src/main/java/com/checkmarx/flow/controller/bitbucket/server/BitbucketServerPushHandler.java
@@ -0,0 +1,126 @@
+package com.checkmarx.flow.controller.bitbucket.server;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+
+import com.checkmarx.flow.constants.FlowConstants;
+import com.checkmarx.flow.dto.BugTracker;
+import com.checkmarx.flow.dto.EventResponse;
+import com.checkmarx.flow.dto.ScanRequest;
+import com.checkmarx.flow.service.BitBucketService;
+import com.checkmarx.flow.utils.HTMLHelper;
+import com.checkmarx.flow.utils.ScanUtils;
+import com.checkmarx.sdk.config.Constants;
+import com.checkmarx.sdk.dto.filtering.FilterConfiguration;
+import com.checkmarx.sdk.dto.sast.CxConfig;
+
+import org.slf4j.Logger;
+import org.slf4j.MDC;
+import org.springframework.http.ResponseEntity;
+
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+public class BitbucketServerPushHandler extends BitbucketServerEventHandler {
+
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(BitbucketServerPushHandler.class);
+
+
+    @Override
+    public ResponseEntity<EventResponse> execute() {
+        String uid = configProvider.getHelperService().getShortUid();
+        MDC.put(FlowConstants.MAIN_MDC_ENTRY, uid);
+        controllerRequest = webhookUtils.ensureNotNull(controllerRequest);
+
+        log.info("Processing BitBucket PUSH request");
+
+        try {
+            // Repository repository = event.getRepository();
+            // String app = repository.getName();
+            if (!ScanUtils.empty(controllerRequest.getApplication())) {
+                application = controllerRequest.getApplication();
+            }
+
+            // set the default bug tracker as per yml
+            webhookUtils.setBugTracker(configProvider.getFlowProperties(), controllerRequest);
+            BugTracker.Type bugType = ScanUtils.getBugTypeEnum(controllerRequest.getBug(),
+                    configProvider.getFlowProperties().getBugTrackerImpl());
+
+            Optional.ofNullable(controllerRequest.getAppOnly())
+                    .ifPresent(configProvider.getFlowProperties()::setTrackApplicationOnly);
+
+            if (ScanUtils.empty(product)) {
+                product = ScanRequest.Product.CX.getProduct();
+            }
+            ScanRequest.Product p = ScanRequest.Product.valueOf(product.toUpperCase(Locale.ROOT));
+            // String currentBranch =
+            // ScanUtils.getBranchFromRef(event.getChanges().get(INDEX_FROM_CHANGES).getRefId());
+            String currentBranch = ScanUtils.getBranchFromRef(branchFromRef);
+            List<String> branches = webhookUtils.getBranches(controllerRequest, configProvider.getFlowProperties());
+            // String latestCommit = event.getChanges().get(INDEX_FROM_CHANGES).getToHash();
+            String latestCommit = toHash;
+
+            BugTracker bt = ScanUtils.getBugTracker(controllerRequest.getAssignee(), bugType,
+                    configProvider.getJiraProperties(), controllerRequest.getBug());
+            FilterConfiguration filter = configProvider.getFilterFactory().getFilter(controllerRequest,
+                    configProvider.getFlowProperties());
+
+            // List<String> emails = new ArrayList<>();
+            // emails.add(event.getActor().getEmailAddress());
+
+            // String gitUrl = getGitUrl(repository);
+            String gitUrl = getGitUrl();
+            String gitAuthUrl = getGitAuthUrl(gitUrl);
+
+            ScanRequest request = ScanRequest.builder().application(application).product(p)
+                    .project(controllerRequest.getProject())
+                    .team(controllerRequest.getTeam())
+                    .namespace(getNamespace())
+                    // .repoName(repository.getName())
+                    .repoName(repositoryName)
+                    .repoUrl(gitUrl)
+                    .repoUrlWithAuth(gitAuthUrl)
+                    .repoType(ScanRequest.Repository.BITBUCKETSERVER)
+                    .branch(currentBranch)
+                    // .refs(event.getChanges().get(0).getRefId())
+                    .refs(refId)
+                    .email(emails)
+                    .scanPreset(controllerRequest.getPreset())
+                    .incremental(controllerRequest.getIncremental())
+                    .excludeFolders(controllerRequest.getExcludeFolders())
+                    .excludeFiles(controllerRequest.getExcludeFiles())
+                    .bugTracker(bt)
+                    .filter(filter)
+                    .hash(latestCommit)
+                    .build();
+
+            // setBrowseUrl(repository, request);
+            setBrowseUrl(request);
+            // fillRequestWithCommonAdditionalData(request, repository, body);
+            fillRequestWithCommonAdditionalData(request, toProjectKey, toSlug, webhookPayload);
+            checkForConfigAsCode(request);
+            request.setId(uid);
+            // only initiate scan/automation if target branch is applicable
+            if (configProvider.getHelperService().isBranch2Scan(request, branches)) {
+                configProvider.getFlowService().initiateAutomation(request);
+            }
+        } catch (IllegalArgumentException e) {
+            return webhookUtils.getBadRequestMessage(e, controllerRequest, product);
+        }
+        return webhookUtils.getSuccessMessage();
+    }
+
+    /*
+     * private void fillRequestWithCommonAdditionalData(ScanRequest request,
+     * Repository_ repository, String hookPayload) {
+     * fillRequestWithCommonAdditionalData (request,
+     * repository.getProject().getKey(), repository.getSlug(), hookPayload); }
+     * 
+     * private void fillRequestWithCommonAdditionalData(ScanRequest request,
+     * Repository repository, String hookPayload) {
+     * fillRequestWithCommonAdditionalData (request,
+     * repository.getProject().getKey(), repository.getSlug(), hookPayload); }
+     */
+
+}
