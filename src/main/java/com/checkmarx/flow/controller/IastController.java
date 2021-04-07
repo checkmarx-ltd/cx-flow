20b4b835bf5f2d39205ea17e50463d267ef21a5c
Andrei Tetka <andrei.tetka@checkmarx.com>
2021-04-07 16:14:08 +0300

    fix integration with IAST

--- src/main/java/com/checkmarx/flow/controller/IastController.java
+++ src/main/java/com/checkmarx/flow/controller/IastController.java
@@ -1,11 +1,16 @@
 package com.checkmarx.flow.controller;
 
 import com.checkmarx.flow.dto.EventResponse;
+import com.checkmarx.flow.service.IastService;
+import com.checkmarx.flow.utils.TokenUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.Random;
 
 import static com.atlassian.sal.api.xsrf.XsrfHeaderValidator.TOKEN_HEADER;
@@ -16,12 +21,16 @@ import static com.atlassian.sal.api.xsrf.XsrfHeaderValidator.TOKEN_HEADER;
 @RequiredArgsConstructor
 public class IastController {
 
-    private Random random = new Random();
+
+    @Autowired
+    private IastService iastService;
+    @Autowired
+    private TokenUtils tokenUtils;
 
     @PostMapping(value = { "/iniqTag"})
     public ResponseEntity<EventResponse> generateUniqTag(){
         return ResponseEntity.accepted().body(EventResponse.builder()
-                .message(Long.toString(Math.abs(random.nextLong())))
+                .message(iastService.generateUniqTag())
                 .success(true)
                 .build());
     }
@@ -31,11 +40,23 @@ public class IastController {
     public ResponseEntity<EventResponse> stopScanAndCreateJiraTask(
             @PathVariable(value = "scanId", required = false) String scanId,
             @RequestHeader(value = TOKEN_HEADER) String token){
-        // Validate shared API token from header
-//        validateToken(token);   //FlowController
+//         Validate shared API token from header
+        tokenUtils.validateToken(token);
+        try {
+            iastService.stopScanAndCreateJiraIssueFromIastSummary(scanId);
+            return ResponseEntity.accepted().body(EventResponse.builder()
+                    .message("OK")
+                    .success(true)
+                    .build());
 
+        } catch (IOException e) {
+            log.warn("Can't stop scan or create jira task", e);
+        }
 
-        return null;
+        return ResponseEntity.status(500).body(EventResponse.builder()
+                .message("Internal Server Error")
+                .success(false)
+                .build());
     }
 
 
