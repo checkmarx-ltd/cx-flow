433090fddd7e9240d678cc739c28594da81df81b
Jeff Armstrong <jarmstrong@nezasoft.com>
2021-08-16 23:07:32 -0700

    - Updated cucumber test.

--- src/test/java/com/checkmarx/flow/cucumber/integration/cxconfig/ConfigAsCodeBranchSteps.java
+++ src/test/java/com/checkmarx/flow/cucumber/integration/cxconfig/ConfigAsCodeBranchSteps.java
@@ -37,7 +37,7 @@ public class ConfigAsCodeBranchSteps {
     private final GitHubAppAuthService gitHubAppAuthService;
     private final GitAuthUrlGenerator gitAuthUrlGenerator;
     private final ShardSessionTracker sessionTrackerMock;
-    private final CxClient cxServiceMock;
+    private final CxService cxServiceMock;
     private String defaultBranch;
     private String actualBranch;
 
