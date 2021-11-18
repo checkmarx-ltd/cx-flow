f857206127631671f1b990800e8e8e3d7c313a16
Samuel Cordeiro <68207834+cx-scord@users.noreply.github.com>
2021-11-18 12:33:11 +0000

    Add extra comments to set commit hash when using automated workflows

--- src/main/resources/samples/ScanComment.groovy
+++ src/main/resources/samples/ScanComment.groovy
@@ -4,7 +4,11 @@ import groovyx.net.http.HTTPBuilder
 
 def repoUrl = request.getRepoUrl()
 def branch = request.getBranch()
+//When using webhooks to read the commit hash, use this:
 def commitId = request.getHash()
+//When using SCM automated workflows, comment the line above and uncomment the 2 lines below so the hash is not null (example is for Github Actions):
+// def env = System.getenv()
+// def commitId = env['GITHUB_SHA']
 
 String scanComment = "Repo: $repoUrl | Branch: $branch | Commit ID: $commitId"
 
