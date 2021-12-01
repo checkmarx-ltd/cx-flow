e33f793a600578f33d14ef08e9f3319470e893b9
Ashish Malik <87957768+ash99d@users.noreply.github.com>
2021-12-01 15:42:45 +1100

    make comment error less verbose
    since these are expected with the scan-submitted-comment disabled

--- src/main/java/com/checkmarx/flow/service/RepoService.java
+++ src/main/java/com/checkmarx/flow/service/RepoService.java
@@ -48,8 +48,7 @@ public abstract class RepoService {
         }
         catch (Exception e) {
             // We "swallow" the exception so that the flow will not be terminated because of errors in GIT comments
-            log.error("Error while adding or updating {} repo pull request comment",
-                      request.getRepoType(), e);
+            log.warn("Error while adding or updating {} repo pull request comment", request.getRepoType());
         }
 
     }
