0773ebc9038384bcdcda412c2e89f20aff1cf622
DhavalPatelPersistent <dhaval_patel@persistent.com>
2021-12-08 21:15:02 +0530

    Add scanResubmit property to config-as-code

--- src/main/java/com/checkmarx/flow/dto/FlowOverride.java
+++ src/main/java/com/checkmarx/flow/dto/FlowOverride.java
@@ -22,6 +22,8 @@ public class FlowOverride {
     public List<String> branches = null;
     @JsonProperty("emails")
     public List<String> emails = null;
+    @JsonProperty("scanResubmit")
+    public String scanResubmit = null;
     @JsonProperty("bugTracker")
     public String bugTracker;
     @JsonProperty("jira")
@@ -58,12 +60,20 @@ public class FlowOverride {
         return this.emails;
     }
 
+    public String getScanResubmit() {
+        return scanResubmit;
+    }
+
     public String getBugTracker() {
         return bugTracker;
     }
 
     public void setBugTracker(String bugTracker) {
         this.bugTracker = bugTracker;
+    }
+
+    public void setScanResubmit(String scanResubmit) {
+        this.scanResubmit = scanResubmit;
     }
 
     public Jira getJira() {
