caa1a8f40f7b2c66e1626ba73c29abd09d1b53be
Leonel Sanches <Leonel.Sanches@checkmarx.com>
2022-03-16 17:06:12 -0700

    Implementing Slack Issue Tracker.

--- src/main/java/com/checkmarx/flow/config/properties/SlackProperties.java
+++ src/main/java/com/checkmarx/flow/config/properties/SlackProperties.java
@@ -5,10 +5,19 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Component
 @ConfigurationProperties(prefix = "slack")
 @Validated
 @Data
 public class SlackProperties {
+    @NotNull
+    @NotBlank
     private String botToken;
+    private String channelName = "random";
+    private String channelScript;
+    private Integer highsThreshold = 0;
+    private Integer mediumsThreshold = 0;
 }
---
+++ src/main/resources/samples/SlackChannelScript.groovy
@@ -0,0 +1,3 @@
+// Here you have the `request` object (ScanRequest) that has all the details
+// sent by the SCM in a Webhook, for instance.
+return "#random"
