43ca02eb18c955e1a3091052c9d1999735231e98
Nidhi Jaiswal <jaiswal.nidhi0512@gmail.com>
2022-07-06 12:54:28 +0530

    Reverting Changes

--- src/main/java/com/checkmarx/flow/CxFlowRunner.java
+++ src/main/java/com/checkmarx/flow/CxFlowRunner.java
@@ -464,20 +464,6 @@ public class CxFlowRunner implements ApplicationRunner {
                         log.warn("Azure DevOps git clone scan not implemented");
                     }
                 } else if (file != null) {
-                    if(gitLabProperties != null && namespace != null && repoName != null && repoUrl != null) {
-                        repoUrl = getNonEmptyRepoUrl(namespace, repoName, repoUrl, gitLabProperties.getGitUri(namespace, repoName));
-                        String token = gitLabProperties.getToken();
-                        gitAuthUrl = repoUrl.replace(Constants.HTTPS, Constants.HTTPS_OAUTH2.concat(token).concat("@"));
-                        gitAuthUrl = gitAuthUrl.replace(Constants.HTTP, Constants.HTTP_OAUTH2.concat(token).concat("@"));
-                        request.setRepoUrlWithAuth(gitAuthUrl);
-                    }
-                    if(gitHubProperties != null && namespace != null && repoName != null && repoUrl != null) {
-                        repoUrl = getNonEmptyRepoUrl(namespace, repoName, repoUrl, gitHubProperties.getGitUri(namespace, repoName));
-                        String token = gitHubProperties.getToken();
-                        gitAuthUrl = repoUrl.replace(Constants.HTTPS, Constants.HTTPS.concat(token).concat("@"));
-                        gitAuthUrl = gitAuthUrl.replace(Constants.HTTP, Constants.HTTP.concat(token).concat("@"));
-                        request.setRepoUrlWithAuth(gitAuthUrl);
-                    }
                     scanLocalPath(request, file);
                 } else {
                     log.error("No valid option was provided for driving scan");
