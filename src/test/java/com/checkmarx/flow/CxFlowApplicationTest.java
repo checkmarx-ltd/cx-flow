8abcb3c5a678a545b88e87277bdb6a4126dc2d81
Satyam Chaurasia <samchaurasia86@gmail.com>
2025-02-18 16:20:07 +0530

    fix for componant test case

--- src/test/java/com/checkmarx/flow/CxFlowApplicationTest.java
+++ src/test/java/com/checkmarx/flow/CxFlowApplicationTest.java
@@ -1,13 +1,17 @@
 package com.checkmarx.flow;
 
+import com.checkmarx.flow.utils.JasyptConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {JasyptConfig.class})
+
 public class CxFlowApplicationTest {
 
     @Test
