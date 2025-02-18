4bbe74b5629da01e3a287b1c1ca443fd63c02658
Satyam Chaurasia <samchaurasia86@gmail.com>
2025-02-18 16:27:36 +0530

    fix for componant test case

--- src/test/java/com/checkmarx/flow/cucumber/component/commentscript/CommentScriptRunner.java
+++ src/test/java/com/checkmarx/flow/cucumber/component/commentscript/CommentScriptRunner.java
@@ -1,10 +1,13 @@
 package com.checkmarx.flow.cucumber.component.commentscript;
 
+import com.checkmarx.flow.utils.JasyptConfig;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
 
 @RunWith(Cucumber.class)
+@ContextConfiguration(classes = {JasyptConfig.class})
 @CucumberOptions(
         features = "src/test/resources/cucumber/features/componentTests/configure-sast-comment-script.feature",
         tags = "not @Skip")
